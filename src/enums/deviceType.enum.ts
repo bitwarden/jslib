0f0b092ed7cc47202a486b8d84ada10258e58396
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-01-06 14:27:30 -0500

    add more enums to jslib

---
+++ src/enums/deviceType.enum.ts
@@ -0,0 +1,8 @@
+export enum DeviceType {
+    Chrome = 2,
+    Firefox = 3,
+    Opera = 4,
+    Edge = 5,
+    Vivaldi = 19,
+    Safari = 20,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { CipherType } from './enums/cipherType.enum';
+export { DeviceType } from './enums/deviceType.enum';
 export { EncryptionType } from './enums/encryptionType.enum';
+export { FieldType } from './enums/fieldType.enum';
+export { SecureNoteType } from './enums/secureNoteType.enum';
