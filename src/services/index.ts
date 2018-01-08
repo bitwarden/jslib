5c243e01eca82ddc169ebe91ae34a9465d043db9
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-01-08 16:22:19 -0500

    fix constants export

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,3 @@
-export { ConstantsService } from './crypto.service';
+export { ConstantsService } from './constants.service';
 export { CryptoService } from './crypto.service';
 export { UtilsService } from './utils.service';
