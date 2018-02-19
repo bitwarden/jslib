5b63336347ad9597ed65d907b1bdb804d8b4f39a
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-02-19 13:07:37 -0500

    Revert "collection from domain index"
    This reverts commit 36315dae19914490e8810746b7935d0596d2a3ba.

--- src/services/collection.service.ts
+++ src/services/collection.service.ts
@@ -1,6 +1,6 @@
 import { CollectionData } from '../models/data/collectionData';
 
-import { Collection } from '../models/domain';
+import { Collection } from '../models/domain/collection';
 
 import { CollectionView } from '../models/view/collectionView';
 
