fa5f6c09068add3951889961f545e6990d9c3ea7
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-04-10 13:53:21 -0400

    return boolean from generate password

--- src/angular/components/add-edit.component.ts
+++ src/angular/components/add-edit.component.ts
@@ -235,17 +235,18 @@ export class AddEditComponent {
         } catch { }
     }
 
-    async generatePassword() {
+    async generatePassword(): Promise<boolean> {
         if (this.cipher.login != null && this.cipher.login.password != null && this.cipher.login.password.length) {
             const confirmed = await this.platformUtilsService.showDialog(
                 this.i18nService.t('overwritePasswordConfirmation'), this.i18nService.t('overwritePassword'),
                 this.i18nService.t('yes'), this.i18nService.t('no'));
             if (!confirmed) {
-                return;
+                return false;
             }
         }
 
         this.onGeneratePassword.emit();
+        return true;
     }
 
     togglePassword() {
