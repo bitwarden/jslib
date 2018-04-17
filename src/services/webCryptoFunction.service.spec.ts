da473ee1a6c7448f1a28d1ae59a04d542f4349cc
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-04-17 18:26:02 -0400

    test web crypto hashing

--- src/services/webCryptoFunction.service.spec.ts
+++ src/services/webCryptoFunction.service.spec.ts
@@ -18,8 +18,34 @@ describe('WebCrypto Function Service', () => {
 
         testPbkdf2ValidKey(false, 'sha256', regular256Key, utf8256Key, unicode256Key);
         testPbkdf2ValidKey(false, 'sha512', regular512Key, utf8512Key, unicode512Key);
+
         testPbkdf2ValidKey(true, 'sha256', regular256Key, utf8256Key, unicode256Key);
         testPbkdf2ValidKey(true, 'sha512', regular512Key, utf8512Key, unicode512Key);
+    });
+
+    describe('hash', () => {
+        const regular1Hash = '2a241604fb921fad12bf877282457268e1dccb70';
+        const utf81Hash = '85672798dc5831e96d6c48655d3d39365a9c88b6';
+        const unicode1Hash = '39c975935054a3efc805a9709b60763a823a6ad4';
+
+        const regular256Hash = '2b8e96031d352a8655d733d7a930b5ffbea69dc25cf65c7bca7dd946278908b2';
+        const utf8256Hash = '25fe8440f5b01ed113b0a0e38e721b126d2f3f77a67518c4a04fcde4e33eeb9d';
+        const unicode256Hash = 'adc1c0c2afd6e92cefdf703f9b6eb2c38e0d6d1a040c83f8505c561fea58852e';
+
+        const regular512Hash = 'c15cf11d43bde333647e3f559ec4193bb2edeaa0e8b902772f514cdf3f785a3f49a6e02a4b87b3' +
+            'b47523271ad45b7e0aebb5cdcc1bc54815d256eb5dcb80da9d';
+        const utf8512Hash = '035c31a877a291af09ed2d3a1a293e69c3e079ea2cecc00211f35e6bce10474ca3ad6e30b59e26118' +
+            '37463f20969c5bc95282965a051a88f8cdf2e166549fcdd';
+        const unicode512Hash = '2b16a5561af8ad6fe414cc103fc8036492e1fc6d9aabe1b655497054f760fe0e34c5d100ac773d' +
+            '9f3030438284f22dbfa20cb2e9b019f2c98dfe38ce1ef41bae';
+
+        testHash(false, 'sha1', regular1Hash, utf81Hash, unicode1Hash);
+        testHash(false, 'sha256', regular256Hash, utf8256Hash, unicode256Hash);
+        testHash(false, 'sha512', regular512Hash, utf8512Hash, unicode512Hash);
+
+        testHash(true, 'sha1', regular1Hash, utf81Hash, unicode1Hash);
+        testHash(true, 'sha256', regular256Hash, utf8256Hash, unicode256Hash);
+        testHash(true, 'sha512', regular512Hash, utf8512Hash, unicode512Hash);
     });
 });
 
@@ -47,8 +73,7 @@ function testPbkdf2ValidKey(edge: boolean, algorithm: 'sha256' | 'sha512', regul
 
     it('should create valid ' + algorithm + ' key from unicode input' + forEdge, async () => {
         const webCryptoFunctionService = getWebCryptoFunctionService(edge);
-        const key = await webCryptoFunctionService.pbkdf2(UtilsService.fromUtf8ToArray(unicodePassword).buffer,
-            UtilsService.fromUtf8ToArray(regularEmail).buffer, algorithm, 5000, 256);
+        const key = await webCryptoFunctionService.pbkdf2(unicodePassword, regularEmail, algorithm, 5000, 256);
         expect(UtilsService.fromBufferToB64(key)).toBe(unicodeKey);
     });
 
@@ -57,6 +82,38 @@ function testPbkdf2ValidKey(edge: boolean, algorithm: 'sha256' | 'sha512', regul
         const key = await webCryptoFunctionService.pbkdf2(UtilsService.fromUtf8ToArray(regularPassword).buffer,
             UtilsService.fromUtf8ToArray(regularEmail).buffer, algorithm, 5000, 256);
         expect(UtilsService.fromBufferToB64(key)).toBe(regularKey);
+    });
+}
+
+function testHash(edge: boolean, algorithm: 'sha1' | 'sha256' | 'sha512', regularHash: string,
+    utf8Hash: string, unicodeHash: string) {
+    const forEdge = edge ? ' for edge' : '';
+    const regularValue = 'HashMe!!';
+    const utf8Value = 'HǻshMe!!';
+    const unicodeValue = '😀HashMe!!!🙏';
+
+    it('should create valid ' + algorithm + ' hash from regular input' + forEdge, async () => {
+        const webCryptoFunctionService = getWebCryptoFunctionService(edge);
+        const hash = await webCryptoFunctionService.hash(regularValue, algorithm);
+        expect(UtilsService.fromBufferToHex(hash)).toBe(regularHash);
+    });
+
+    it('should create valid ' + algorithm + ' hash from utf8 input' + forEdge, async () => {
+        const webCryptoFunctionService = getWebCryptoFunctionService(edge);
+        const hash = await webCryptoFunctionService.hash(utf8Value, algorithm);
+        expect(UtilsService.fromBufferToHex(hash)).toBe(utf8Hash);
+    });
+
+    it('should create valid ' + algorithm + ' hash from unicode input' + forEdge, async () => {
+        const webCryptoFunctionService = getWebCryptoFunctionService(edge);
+        const hash = await webCryptoFunctionService.hash(unicodeValue, algorithm);
+        expect(UtilsService.fromBufferToHex(hash)).toBe(unicodeHash);
+    });
+
+    it('should create valid ' + algorithm + ' hash from array buffer input' + forEdge, async () => {
+        const webCryptoFunctionService = getWebCryptoFunctionService(edge);
+        const hash = await webCryptoFunctionService.hash(UtilsService.fromUtf8ToArray(regularValue).buffer, algorithm);
+        expect(UtilsService.fromBufferToHex(hash)).toBe(regularHash);
     });
 }
 
