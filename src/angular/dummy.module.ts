983a7b474c344475edc3290d0ce1894a92794752
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-04-26 00:43:01 -0400

    dummy module. ref https://github.com/angular/angular/issues/13590

---
+++ src/angular/dummy.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { InputVerbatimDirective } from './directives/input-verbatim.directive';
+
+@NgModule({
+    imports: [],
+    declarations: [
+        InputVerbatimDirective,
+    ],
+})
+export class DummyModule {
+}
