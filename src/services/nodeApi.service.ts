f173001a411acb39c0d2ebb11d17ea90d70ec784
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-05-15 09:02:57 -0400

    polyfill fetch globals on nodeapi

--- src/services/nodeApi.service.ts
+++ src/services/nodeApi.service.ts
@@ -1,11 +1,14 @@
-import { Utils } from '../misc/utils';
+import * as fe from 'node-fetch';
 
 import { ApiService } from './api.service';
 
 import { PlatformUtilsService } from '../abstractions/platformUtils.service';
 import { TokenService } from '../abstractions/token.service';
 
-import * as fetch from 'node-fetch';
+(global as any).fetch = fe.default;
+(global as any).Request = fe.Request;
+(global as any).Response = fe.Response;
+(global as any).Headers = fe.Headers;
 
 export class NodeApiService extends ApiService {
     constructor(tokenService: TokenService, platformUtilsService: PlatformUtilsService,
