fe3a8785422a8866dbcb91d31db6d3fd2aa2daaa
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-06-08 16:07:21 -0400

    update dummy module

--- src/angular/dummy.module.ts
+++ src/angular/dummy.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { InputVerbatimDirective } from './directives/input-verbatim.directive';
+import { TrueFalseValueDirective } from './directives/true-false-value.directive';
 
 @NgModule({
     imports: [],
     declarations: [
         InputVerbatimDirective,
+        TrueFalseValueDirective,
     ],
 })
 export class DummyModule {
