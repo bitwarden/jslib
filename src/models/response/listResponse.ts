ec505b8c5508e4c8f6191c946b042a804c73c501
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-06-26 22:52:12 -0400

    data is never null

--- src/models/response/listResponse.ts
+++ src/models/response/listResponse.ts
@@ -3,7 +3,7 @@ export class ListResponse<T> {
     continuationToken: string;
 
     constructor(response: any, t: new (dataResponse: any) => T) {
-        this.data = response.Data.map((dr: any) => new t(dr));
+        this.data = response.Data == null ? [] : response.Data.map((dr: any) => new t(dr));
         this.continuationToken = response.ContinuationToken;
     }
 }
