7ba04c919eb64f321347a1c38810db3432cb3141
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-02 15:19:33 -0400

    plan types

---
+++ src/enums/planType.ts
@@ -0,0 +1,9 @@
+export enum PlanType {
+    Free = 0,
+    FamiliesAnnually = 1,
+    TeamsMonthly = 2,
+    TeamsAnnually = 3,
+    EnterpriseMonthly = 4,
+    EnterpriseAnnually = 5,
+    Custom = 6,
+}
