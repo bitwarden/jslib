2d612b07de588a52e0e5895c4a4593f670442bb9
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-09-10 08:22:10 -0400

    no key safety check for orgs

--- src/services/cipher.service.ts
+++ src/services/cipher.service.ts
@@ -134,6 +134,9 @@ export class CipherService implements CipherServiceAbstraction {
 
         if (key == null && cipher.organizationId != null) {
             key = await this.cryptoService.getOrgKey(cipher.organizationId);
+            if (key == null) {
+                throw new Error('Cannot encrypt cipher for organization. No key.');
+            }
         }
         await Promise.all([
             this.encryptObjProperty(model, cipher, {
