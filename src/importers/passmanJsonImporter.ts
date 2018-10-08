0124cb22a22a9ba16be1e34d536ec72c91512d61
Severin Leonhardt <serviushack@gmx.de>
2018-10-08 20:37:30 +0200

    Fix indentation

--- src/importers/passmanJsonImporter.ts
+++ src/importers/passmanJsonImporter.ts
@@ -13,38 +13,38 @@ export class PassmanJsonImporter extends BaseImporter implements Importer {
         }
 
         results.forEach((credential: any) => {
-          if (credential.tags.length > 0) {
+            if (credential.tags.length > 0) {
                 const folderName = credential.tags[0].text;
                 this.processFolder(result, folderName);
-          }
+            }
 
-                const cipher = this.initLoginCipher();
-                cipher.name = credential.label;
-                if (cipher.name.length > 30) {
-                    cipher.name = cipher.name.substring(0, 30);
-                }
-                cipher.login.username = this.getValueOrDefault(credential.username, credential.email);
-                cipher.login.password = this.getValueOrDefault(credential.password);
-                cipher.login.uris = this.makeUriArray(credential.url);
-                cipher.notes = this.getValueOrDefault(credential.description);
-                cipher.revisionDate = new Date(credential.changed * 1000);
+            const cipher = this.initLoginCipher();
+            cipher.name = credential.label;
+            if (cipher.name.length > 30) {
+                cipher.name = cipher.name.substring(0, 30);
+            }
+            cipher.login.username = this.getValueOrDefault(credential.username, credential.email);
+            cipher.login.password = this.getValueOrDefault(credential.password);
+            cipher.login.uris = this.makeUriArray(credential.url);
+            cipher.notes = this.getValueOrDefault(credential.description);
+            cipher.revisionDate = new Date(credential.changed * 1000);
 
-                if (credential.otp) {
+            if (credential.otp) {
                   cipher.login.totp = credential.otp.secret;
-                }
+            }
 
-                credential.custom_fields.forEach((custom_field: any) => {
-                  switch (custom_field.field_type) {
+            credential.custom_fields.forEach((custom_field: any) => {
+                switch (custom_field.field_type) {
                     case "text":
                     case "password":
-                      this.processKvp(cipher, custom_field.label, custom_field.value);
-                      break;
-                  }
-                });
+                        this.processKvp(cipher, custom_field.label, custom_field.value);
+                        break;
+                }
+            });
 
-                this.convertToNoteIfNeeded(cipher);
-                this.cleanupCipher(cipher);
-                result.ciphers.push(cipher);
+            this.convertToNoteIfNeeded(cipher);
+            this.cleanupCipher(cipher);
+            result.ciphers.push(cipher);
         });
 
         if (this.organization) {
