3ca1544eb38c85f3b08c194c275d7db447a82673
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-10-09 16:00:50 -0400

    renamed event to updated2fa

--- src/enums/eventType.ts
+++ src/enums/eventType.ts
@@ -1,7 +1,7 @@
 export enum EventType {
     User_LoggedIn = 1000,
     User_ChangedPassword = 1001,
-    User_Enabled2fa = 1002,
+    User_Updated2fa = 1002,
     User_Disabled2fa = 1003,
     User_Recovered2fa = 1004,
     User_FailedLogIn = 1005,
