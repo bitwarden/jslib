b48c48470801eb88aaec2c2cd84baeb6f1876923
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-11-15 15:27:04 -0500

    misc score props moved to jslib

--- src/angular/components/register.component.ts
+++ src/angular/components/register.component.ts
@@ -32,6 +32,36 @@ export class RegisterComponent {
         protected platformUtilsService: PlatformUtilsService,
         protected passwordGenerationService: PasswordGenerationService) { }
 
+    get masterPasswordScoreWidth() {
+        return this.masterPasswordScore == null ? 0 : (this.masterPasswordScore + 1) * 20;
+    }
+
+    get masterPasswordScoreColor() {
+        switch (this.masterPasswordScore) {
+            case 4:
+                return 'success';
+            case 3:
+                return 'primary';
+            case 2:
+                return 'warning';
+            default:
+                return 'danger';
+        }
+    }
+
+    get masterPasswordScoreText() {
+        switch (this.masterPasswordScore) {
+            case 4:
+                return this.i18nService.t('strong');
+            case 3:
+                return this.i18nService.t('good');
+            case 2:
+                return this.i18nService.t('weak');
+            default:
+                return this.masterPasswordScore != null ? this.i18nService.t('weak') : null;
+        }
+    }
+
     async submit() {
         if (this.email == null || this.email === '') {
             this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
