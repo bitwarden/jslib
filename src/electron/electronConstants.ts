ed86b9d83c7e8ec00bac1463f550bddcafd25453
Christoph Haas <christoph.h@sprinternet.at>
2018-11-25 15:32:31 +0100

    Allow app to start minimized (as tray icon)

--- src/electron/electronConstants.ts
+++ src/electron/electronConstants.ts
@@ -1,4 +1,5 @@
 export class ElectronConstants {
     static readonly enableMinimizeToTrayKey: string = 'enableMinimizeToTray';
     static readonly enableTrayKey: string = 'enableTray';
+    static readonly enableStartMinimizedKey: string = 'enableStartMinimizedKey';
 }
