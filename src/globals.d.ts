dce1453732553d991ac910e3a93038f7331c8ab9
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-01-27 09:02:41 -0500

    fix HttpsProxyAgent typings

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,28 +1,3 @@
 declare function escape(s: string): string;
 declare function unescape(s: string): string;
 declare module 'duo_web_sdk';
-
-// From: https://github.com/TooTallNate/node-https-proxy-agent/issues/27
-declare module 'https-proxy-agent' {
-    import * as https from 'https'
-
-    namespace HttpsProxyAgent {
-        interface HttpsProxyAgentOptions {
-            host: string
-            port: number
-            secureProxy?: boolean
-            headers?: {
-                [key: string]: string
-            }
-            [key: string]: any
-        }
-    }
-
-    // HttpsProxyAgent doesnt *actually* extend https.Agent, but for my purposes I want it to pretend that it does
-    class HttpsProxyAgent extends https.Agent {
-        constructor(opts: string)
-        constructor(opts: HttpsProxyAgent.HttpsProxyAgentOptions)
-    }
-
-    export = HttpsProxyAgent
-}
