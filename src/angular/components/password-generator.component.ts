a6f304781b9c526cb6a12bdad5dbae6642f49acb
Vincent Salucci <vsalucci@bitwarden.com>
2020-02-25 13:13:55 -0600

    Enforce Password Generator Policy

--- src/angular/components/password-generator.component.ts
+++ src/angular/components/password-generator.component.ts
@@ -8,6 +8,9 @@ import {
 import { I18nService } from '../../abstractions/i18n.service';
 import { PasswordGenerationService } from '../../abstractions/passwordGeneration.service';
 import { PlatformUtilsService } from '../../abstractions/platformUtils.service';
+import { PolicyService } from '../../abstractions/policy.service';
+import { PolicyType } from '../../enums/policyType';
+import { Policy } from '../../models/domain/policy';
 
 export class PasswordGeneratorComponent implements OnInit {
     @Input() showSelect: boolean = false;
@@ -17,15 +20,23 @@ export class PasswordGeneratorComponent implements OnInit {
     password: string = '-';
     showOptions = false;
     avoidAmbiguous = false;
+    enforcedMinLength = 0;
+    hasEnforcedUppercase = false;
+    hasEnforcedLowercase = false;
+    hasEnforcedNumbers = false;
+    enforcedNumberCount = 0;
+    hasEnforcedSpecial = false;
+    enforcedSpecialCount = 0;
 
     constructor(protected passwordGenerationService: PasswordGenerationService,
         protected platformUtilsService: PlatformUtilsService, protected i18nService: I18nService,
-        private win: Window) { }
+        protected policyService: PolicyService, private win: Window) { }
 
     async ngOnInit() {
         this.options = await this.passwordGenerationService.getOptions();
         this.avoidAmbiguous = !this.options.ambiguous;
         this.options.type = this.options.type === 'passphrase' ? 'passphrase' : 'password';
+        this.enforcePolicies(await this.policyService.getAll(PolicyType.PasswordGenerator));
         this.password = await this.passwordGenerationService.generatePassword(this.options);
         this.platformUtilsService.eventTrack('Generated Password');
         await this.passwordGenerationService.addHistory(this.password);
@@ -74,6 +85,55 @@ export class PasswordGeneratorComponent implements OnInit {
         this.showOptions = !this.showOptions;
     }
 
+    protected enforcePolicies(policies: Policy[]) {
+        if (policies == null || policies.length === 0) {
+            return;
+        }
+
+        policies.forEach((currentPolicy) => {
+            if (currentPolicy.enabled && currentPolicy.data != null) {
+                const currentPolicyData = currentPolicy.data;
+
+                if (currentPolicyData.minLength != null && currentPolicyData.minLength > 0
+                    && currentPolicyData.minLength > this.enforcedMinLength) {
+                    this.enforcedMinLength = currentPolicyData.minLength;
+                }
+
+                if (currentPolicyData.useUpper && !this.hasEnforcedUppercase) {
+                    this.options.uppercase = this.hasEnforcedUppercase = true;
+                }
+
+                if (currentPolicyData.useLower && !this.hasEnforcedLowercase) {
+                    this.options.lowercase = this.hasEnforcedLowercase = true;
+                }
+
+                if (currentPolicyData.useNumbers && !this.hasEnforcedNumbers) {
+                    this.options.number = this.hasEnforcedNumbers = true;
+                }
+
+                if (currentPolicyData.minNumbers != null && currentPolicyData.minNumbers > 0
+                    && currentPolicyData.minNumbers > this.enforcedNumberCount) {
+                    this.enforcedNumberCount = currentPolicyData.minNumbers;
+                }
+
+                if (currentPolicyData.useSpecial && !this.hasEnforcedSpecial) {
+                    this.options.special = this.hasEnforcedSpecial = true;
+                }
+
+                if (currentPolicyData.minSpecial != null && currentPolicyData.minSpecial > 0
+                    && currentPolicyData.minSpecial > this.enforcedSpecialCount) {
+                    this.enforcedSpecialCount = currentPolicyData.minSpecial;
+                }
+            }
+        });
+
+        if (this.enforcedMinLength > 0 || this.hasEnforcedUppercase || this.hasEnforcedLowercase
+            || this.hasEnforcedNumbers || this.enforcedNumberCount > 0 || this.hasEnforcedSpecial
+            || this.enforcedSpecialCount > 0) {
+            this.saveOptions(false);
+        }
+    }
+
     private normalizeOptions() {
         this.options.minLowercase = 0;
         this.options.minUppercase = 0;
@@ -95,6 +155,10 @@ export class PasswordGeneratorComponent implements OnInit {
             this.options.length = 128;
         }
 
+        if (this.options.length < this.enforcedMinLength) {
+            this.options.length = this.enforcedMinLength;
+        }
+
         if (!this.options.minNumber) {
             this.options.minNumber = 0;
         } else if (this.options.minNumber > this.options.length) {
@@ -103,12 +167,20 @@ export class PasswordGeneratorComponent implements OnInit {
             this.options.minNumber = 9;
         }
 
+        if (this.options.minNumber < this.enforcedNumberCount) {
+            this.options.minNumber = this.enforcedNumberCount;
+        }
+
         if (!this.options.minSpecial) {
             this.options.minSpecial = 0;
         } else if (this.options.minSpecial > this.options.length) {
             this.options.minSpecial = this.options.length;
         } else if (this.options.minSpecial > 9) {
             this.options.minSpecial = 9;
+        }
+
+        if (this.options.minSpecial < this.enforcedSpecialCount) {
+            this.options.minSpecial = this.enforcedSpecialCount;
         }
 
         if (this.options.minSpecial + this.options.minNumber > this.options.length) {
