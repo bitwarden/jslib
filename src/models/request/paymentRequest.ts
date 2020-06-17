01fd7050818c1ff9b48790c251fd9d91fcb605e1
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2020-06-17 19:46:28 -0400

    extend payment request to collect tax info

--- src/models/request/paymentRequest.ts
+++ src/models/request/paymentRequest.ts
@@ -1,6 +1,7 @@
 import { PaymentMethodType } from '../../enums/paymentMethodType';
+import { OrganizationTaxInfoUpdateRequest } from '../request/organizationTaxInfoUpdateRequest';
 
-export class PaymentRequest {
+export class PaymentRequest extends OrganizationTaxInfoUpdateRequest {
     paymentMethodType: PaymentMethodType;
     paymentToken: string;
 }
