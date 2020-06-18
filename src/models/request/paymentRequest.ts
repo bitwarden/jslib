983f0cbfa7665265f6d449c9ced131b9e3426c6c
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2020-06-17 20:09:47 -0400

    re-order import

--- src/models/request/paymentRequest.ts
+++ src/models/request/paymentRequest.ts
@@ -1,5 +1,5 @@
-import { OrganizationTaxInfoUpdateRequest } from '../request/organizationTaxInfoUpdateRequest';
 import { PaymentMethodType } from '../../enums/paymentMethodType';
+import { OrganizationTaxInfoUpdateRequest } from '../request/organizationTaxInfoUpdateRequest';
 
 export class PaymentRequest extends OrganizationTaxInfoUpdateRequest {
     paymentMethodType: PaymentMethodType;
