eac44048455fb809cf62656731d9a7b6764b3ccb
Hinton <hinton.oscar@gmail.com>
2020-07-02 00:15:22 +0200

    Switch to use windows.security.credentials.ui UserConsentVerifier

--- src/electron/biometric.main.ts
+++ src/electron/biometric.main.ts
@@ -1,16 +1,20 @@
-import {
-    KeyCredentialCreationOption,
-    KeyCredentialManager,
-    KeyCredentialStatus,
-} from '@nodert-win10-rs4/windows.security.credentials';
 import { ipcMain } from 'electron';
 import * as util from 'util';
 import { StorageService } from '../abstractions';
 import { ElectronConstants } from './electronConstants';
+import {
+    UserConsentVerifier,
+    UserConsentVerifierAvailability,
+    UserConsentVerificationResult,
+} from '@nodert-win10-rs4/windows.security.credentials.ui';
 
-const requestCreateAsync = util.promisify(
-    KeyCredentialManager.requestCreateAsync,
-);
+const requestVerification: any = util.promisify(UserConsentVerifier.requestVerificationAsync);
+const checkAvailability: any= util.promisify(UserConsentVerifier.checkAvailabilityAsync);
+
+const allowedAvailabilities = [
+    UserConsentVerifierAvailability.available,
+    UserConsentVerifierAvailability.deviceBusy
+]
 
 export class BiometricMain {
     constructor(private storageService: StorageService) {}
@@ -23,27 +27,15 @@ export class BiometricMain {
         });
     }
 
-    supportsBiometric(): Promise<boolean> {
-        return new Promise((resolve, reject) => {
-            KeyCredentialManager.isSupportedAsync(
-                (error: Error, result: boolean) => resolve(result),
-            );
-        });
+    async supportsBiometric(): Promise<boolean> {
+        const availability = await checkAvailability();
+
+        return allowedAvailabilities.includes(availability)
     }
 
     async requestCreate(): Promise<boolean> {
-        return new Promise((resolve, reject) => {
-            KeyCredentialManager.requestCreateAsync(
-                'bitwarden',
-                KeyCredentialCreationOption.replaceExisting,
-                (error, resp) => {
-                    if (resp.status === KeyCredentialStatus.success) {
-                        resolve(true);
-                    } else {
-                        resolve(false);
-                    }
-                },
-            );
-        });
+        const verification = await requestVerification("Please authenticate yourself to unlock the Bitwarden Vault.");
+
+        return verification === UserConsentVerificationResult.verified;
     }
 }
