dad0e2ce6e4880dd1288ae28e9793a2d78fb11b3
Addison Beck <addisonbeck@MacBook-Pro.local>
2020-07-10 15:40:32 -0500

    fixed a formatting issue

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -165,7 +165,7 @@ export class SearchService implements SearchServiceAbstraction {
                 return true;
             }
 
-            if(c.hasFields) {
+            if (c.hasFields) {
                 for (let field of c.fields) {
                     if (field.value.toLowerCase().indexOf(query) > -1) {
                         return true;
