bb5a29107c47a242a995740dc4439736d05446bb
Vincent Salucci <vincesalucci21@gmail.com>
2020-07-31 13:08:54 -0500

    Fixed sso lint error

--- src/angular/components/sso.component.ts
+++ src/angular/components/sso.component.ts
@@ -96,7 +96,7 @@ export class SsoComponent {
                     this.router.navigate([this.twoFactorRoute], {
                         queryParams: {
                             resetMasterPassword: response.resetMasterPassword,
-                        }
+                        },
                     });
                 }
             } else if (response.resetMasterPassword) {
