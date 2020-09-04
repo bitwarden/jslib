e9cbe0d53910c84137101fa7fbe9805bcd1bdd39
Addison Beck <addisonbeck1@gmail.com>
2020-09-04 15:59:25 -0400

    reverted codeVerifier length back to 64

--- src/angular/components/sso.component.ts
+++ src/angular/components/sso.component.ts
@@ -77,7 +77,7 @@ export class SsoComponent {
 
         const passwordOptions: any = {
             type: 'password',
-            length: 32,
+            length: 64,
             uppercase: true,
             lowercase: true,
             numbers: true,
