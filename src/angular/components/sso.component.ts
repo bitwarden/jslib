cc12e58ac5abb6dfc58e98dd6675ac5c91457137
Addison Beck <abeck@bitwarden.com>
2020-09-04 14:08:18 -0400

    updated sso password string to be 32 characters (#164)

--- src/angular/components/sso.component.ts
+++ src/angular/components/sso.component.ts
@@ -77,7 +77,7 @@ export class SsoComponent {
 
         const passwordOptions: any = {
             type: 'password',
-            length: 64,
+            length: 32,
             uppercase: true,
             lowercase: true,
             numbers: true,
