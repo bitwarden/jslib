595215a9dab97020e680ba8b4fe638017f2951e1
Vincent Salucci <26154748+vincentsalucci@users.noreply.github.com>
2020-10-13 12:06:51 -0500

    [Require SSO] Add policy type enumeration (#186)
    * Added SsoAuthentication policy type
    
    * Updated policy type name // added comments for clarification of what each type controls

--- src/enums/policyType.ts
+++ src/enums/policyType.ts
@@ -1,6 +1,7 @@
 export enum PolicyType {
-    TwoFactorAuthentication = 0,
-    MasterPassword = 1,
-    PasswordGenerator = 2,
-    OnlyOrg = 3,
+    TwoFactorAuthentication = 0, // Requires users to have 2fa enabled
+    MasterPassword = 1, // Sets minimum requirements for master password complexity
+    PasswordGenerator = 2, // Sets minimum requirements/default type for generated passwords/passphrases
+    OnlyOrg = 3, // Allows users to only be apart of one organization
+    RequireSso = 4, // Requires users to authenticate with SSO
 }
