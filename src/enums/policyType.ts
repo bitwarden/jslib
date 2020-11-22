a3c7ceba2e3a039cdd70336fba96f016d114da8e
Vincent Salucci <vincesalucci21@gmail.com>
2020-11-22 14:16:36 -0600

    Updated policy type enum

--- src/enums/policyType.ts
+++ src/enums/policyType.ts
@@ -4,4 +4,5 @@ export enum PolicyType {
     PasswordGenerator = 2, // Sets minimum requirements/default type for generated passwords/passphrases
     SingleOrg = 3, // Allows users to only be apart of one organization
     RequireSso = 4, // Requires users to authenticate with SSO
+    PersonalOwnership = 5, // Disables personal vault ownership for adding/cloning items
 }
