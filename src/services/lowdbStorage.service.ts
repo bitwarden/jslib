998eabd243b8b67861e55f8cbb49ab448afae86b
Matt Gibson <mgibson@bitwarden.com>
2021-02-12 14:04:06 -0600

    Lock lowdb storage file to avoid dirty data collisions

--- src/services/lowdbStorage.service.ts
+++ src/services/lowdbStorage.service.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as lowdb from 'lowdb';
 import * as FileSync from 'lowdb/adapters/FileSync';
 import * as path from 'path';
+import * as lock from 'proper-lockfile';
 
 import { LogService } from '../abstractions/log.service';
 import { StorageService } from '../abstractions/storage.service';
@@ -14,7 +15,8 @@ export class LowdbStorageService implements StorageService {
     private defaults: any;
     private dataFilePath: string;
 
-    constructor(private logService: LogService, defaults?: any, dir?: string, private allowCache = false) {
+    constructor(private logService: LogService, defaults?: any, dir?: string, private allowCache = false,
+        private requireLock = false) {
         this.defaults = defaults;
 
         this.logService.info('Initializing lowdb storage service.');
@@ -26,18 +28,22 @@ export class LowdbStorageService implements StorageService {
                 this.logService.info(`Created dir "${dir}".`);
             }
             this.dataFilePath = path.join(dir, 'data.json');
-            if (!fs.existsSync(this.dataFilePath)) {
-                this.logService.warning(`Could not find data file, "${this.dataFilePath}"; creating it instead.`);
-                fs.writeFileSync(this.dataFilePath, '', { mode: 0o600 });
-                fs.chmodSync(this.dataFilePath, 0o600);
-                this.logService.info(`Created data file "${this.dataFilePath}" with chmod 600.`);
-            }
+            this.lockDbFile(() => {
+                if (!fs.existsSync(this.dataFilePath)) {
+                    this.logService.warning(`Could not find data file, "${this.dataFilePath}"; creating it instead.`);
+                    fs.writeFileSync(this.dataFilePath, '', { mode: 0o600 });
+                    fs.chmodSync(this.dataFilePath, 0o600);
+                    this.logService.info(`Created data file "${this.dataFilePath}" with chmod 600.`);
+                } else {
+                    this.logService.info(`db file "${this.dataFilePath} already exists"; using existing db`);
+                }
+            });
             adapter = new FileSync(this.dataFilePath);
         }
         try {
             this.logService.info('Attempting to create lowdb storage adapter.');
             this.db = lowdb(adapter);
-            this.logService.info('Successfuly created lowdb storage adapter.');
+            this.logService.info('Successfully created lowdb storage adapter.');
         } catch (e) {
             if (e instanceof SyntaxError) {
                 this.logService.warning(`Error creating lowdb storage adapter, "${e.message}"; emptying data file.`);
@@ -52,37 +58,64 @@ export class LowdbStorageService implements StorageService {
 
     init() {
         if (this.defaults != null) {
-            this.logService.info('Writing defaults.');
-            this.readForNoCache();
-            this.db.defaults(this.defaults).write();
-            this.logService.info('Successfully wrote defaults to db.');
+            this.lockDbFile(() => {
+                this.logService.info('Writing defaults.');
+                this.readForNoCache();
+                this.db.defaults(this.defaults).write();
+                this.logService.info('Successfully wrote defaults to db.');
+            });
         }
     }
 
     get<T>(key: string): Promise<T> {
-        this.readForNoCache();
-        const val = this.db.get(key).value();
-        if (val == null) {
-            return Promise.resolve(null);
-        }
-        return Promise.resolve(val as T);
+        return this.lockDbFile(() => {
+            this.readForNoCache();
+            const val = this.db.get(key).value();
+            this.logService.debug(`Successfully read ${key} from db`);
+            if (val == null) {
+                return Promise.resolve(null);
+            }
+            return Promise.resolve(val as T);
+        });
     }
 
     save(key: string, obj: any): Promise<any> {
-        this.readForNoCache();
-        this.db.set(key, obj).write();
-        return Promise.resolve();
+        return this.lockDbFile(() => {
+            this.readForNoCache();
+            this.db.set(key, obj).write();
+            this.logService.debug(`Successfully wrote ${key} to db`);
+            return Promise.resolve();
+        });
     }
 
     remove(key: string): Promise<any> {
-        this.readForNoCache();
-        this.db.unset(key).write();
-        return Promise.resolve();
+        return this.lockDbFile(() => {
+            this.readForNoCache();
+            this.db.unset(key).write();
+            this.logService.debug(`Successfully removed ${key} from db`);
+            return Promise.resolve();
+        });
     }
 
     private readForNoCache() {
         if (!this.allowCache) {
             this.db.read();
+        }
+    }
+
+    private lockDbFile<T>(action: () => T): T {
+        if (this.requireLock && !Utils.isNullOrWhitespace(this.dataFilePath)) {
+            this.logService.debug('acquiring db file lock');
+            let release: () => void;
+            try {
+                release = lock.lockSync(this.dataFilePath);
+                return action();
+            } finally {
+                release();
+                this.logService.debug('db file lock released');
+            }
+        } else {
+            return action();
         }
     }
 }
