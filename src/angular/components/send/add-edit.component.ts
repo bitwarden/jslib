703d0ced6093119e4c612a198f1100d61f5276c6
Vlad <vladgamebox7@gmail.com>
2021-02-26 19:25:15 +0300

    revert accidental revert

--- src/angular/components/send/add-edit.component.ts
+++ src/angular/components/send/add-edit.component.ts
@@ -51,6 +51,7 @@ export class AddEditComponent implements OnInit {
     expirationDateSelect: number = null;
     canAccessPremium = true;
     premiumRequiredAlertShown = false;
+    showOptions = false;
 
     private webVaultUrl: string;
 
@@ -218,16 +219,16 @@ export class AddEditComponent implements OnInit {
         }
     }
 
-    async delete(): Promise<void> {
+    async delete(): Promise<boolean> {
         if (this.deletePromise != null) {
-            return;
+            return false;
         }
         const confirmed = await this.platformUtilsService.showDialog(
             this.i18nService.t('deleteSendConfirmation'),
             this.i18nService.t('deleteSend'),
             this.i18nService.t('yes'), this.i18nService.t('no'), 'warning');
         if (!confirmed) {
-            return;
+            return false;
         }
 
         try {
@@ -236,7 +237,10 @@ export class AddEditComponent implements OnInit {
             this.platformUtilsService.showToast('success', null, this.i18nService.t('deletedSend'));
             await this.load();
             this.onDeletedSend.emit(this.send);
+            return true;
         } catch { }
+
+        return false;
     }
 
     typeChanged() {
@@ -244,6 +248,10 @@ export class AddEditComponent implements OnInit {
             this.premiumRequiredAlertShown = true;
             this.messagingService.send('premiumRequired');
         }
+    }
+
+    toggleOptions() {
+        this.showOptions = !this.showOptions;
     }
 
     protected async loadSend(): Promise<Send> {
