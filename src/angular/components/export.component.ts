08fbdbe8d8b73ca4f54974cbb66d0f64c2e42c85
Thomas Rittson <trittson@bitwarden.com>
2021-03-04 08:31:51 +1000

    Add extra warnings about encrypted export

--- src/angular/components/export.component.ts
+++ src/angular/components/export.component.ts
@@ -29,16 +29,27 @@ export class ExportComponent {
     }
 
     async submit() {
+        let acceptedWarning;
+
         if (this.masterPassword == null || this.masterPassword === '') {
             this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
                 this.i18nService.t('invalidMasterPassword'));
             return;
         }
 
-        const acceptedWarning = await this.platformUtilsService.showDialog(
-            this.i18nService.t(this.encryptedFormat ? 'encExportWarningDesc' : 'exportWarningDesc'),
-            this.i18nService.t('confirmVaultExport'), this.i18nService.t('exportVault'),
-            this.i18nService.t('cancel'), 'warning');
+        if (this.encryptedFormat) {
+            acceptedWarning = await this.platformUtilsService.showDialog(
+                '<p>' + this.i18nService.t('encExportKeyWarningDesc') +
+                '<p>' + this.i18nService.t('encExportAccountWarningDesc'),
+                this.i18nService.t('confirmVaultExport'), this.i18nService.t('exportVault'),
+                this.i18nService.t('cancel'), 'warning',
+                true);
+        } else {
+            acceptedWarning = await this.platformUtilsService.showDialog(
+                this.i18nService.t('exportWarningDesc'),
+                this.i18nService.t('confirmVaultExport'), this.i18nService.t('exportVault'),
+                this.i18nService.t('cancel'), 'warning');
+        }
 
         if (!acceptedWarning) {
             return;
