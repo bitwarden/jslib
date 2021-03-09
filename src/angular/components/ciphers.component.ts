577635f292ef3656a4179ee7227d6abf2faaec56
Thomas Rittson <trittson@bitwarden.com>
2021-03-09 11:47:40 +1000

    Remove ngx-infinite-scroll

--- src/angular/components/ciphers.component.ts
+++ src/angular/components/ciphers.component.ts
@@ -19,7 +19,6 @@ export class CiphersComponent {
 
     loaded: boolean = false;
     ciphers: CipherView[] = [];
-    pagedCiphers: CipherView[] = [];
     searchText: string;
     searchPlaceholder: string = null;
     filter: (cipher: CipherView) => boolean = null;
@@ -27,10 +26,8 @@ export class CiphersComponent {
 
     protected searchPending = false;
     protected didScroll = false;
-    protected pageSize = 100;
 
     private searchTimeout: any = null;
-    private pagedCiphersCount = 0;
     private refreshing = false;
 
     constructor(protected searchService: SearchService) { }
@@ -39,22 +36,6 @@ export class CiphersComponent {
         this.deleted = deleted || false;
         await this.applyFilter(filter);
         this.loaded = true;
-    }
-
-    loadMore() {
-        if (this.ciphers.length <= this.pageSize) {
-            return;
-        }
-        const pagedLength = this.pagedCiphers.length;
-        let pagedSize = this.pageSize;
-        if (this.refreshing && pagedLength === 0 && this.pagedCiphersCount > this.pageSize) {
-            pagedSize = this.pagedCiphersCount;
-        }
-        if (this.ciphers.length > pagedLength) {
-            this.pagedCiphers = this.pagedCiphers.concat(this.ciphers.slice(pagedLength, pagedLength + pagedSize));
-        }
-        this.pagedCiphersCount = this.pagedCiphers.length;
-        this.didScroll = this.pagedCiphers.length > this.pageSize;
     }
 
     async reload(filter: (cipher: CipherView) => boolean = null, deleted: boolean = false) {
@@ -85,13 +66,11 @@ export class CiphersComponent {
         const deletedFilter: (cipher: CipherView) => boolean = c => c.isDeleted === this.deleted;
         if (timeout == null) {
             this.ciphers = await this.searchService.searchCiphers(this.searchText, [this.filter, deletedFilter], indexedCiphers);
-            await this.resetPaging();
             return;
         }
         this.searchPending = true;
         this.searchTimeout = setTimeout(async () => {
             this.ciphers = await this.searchService.searchCiphers(this.searchText, [this.filter, deletedFilter], indexedCiphers);
-            await this.resetPaging();
             this.searchPending = false;
         }, timeout);
     }
@@ -114,18 +93,5 @@ export class CiphersComponent {
 
     isSearching() {
         return !this.searchPending && this.searchService.isSearchable(this.searchText);
-    }
-
-    isPaging() {
-        const searching = this.isSearching();
-        if (searching && this.didScroll) {
-            this.resetPaging();
-        }
-        return !searching && this.ciphers.length > this.pageSize;
-    }
-
-    async resetPaging() {
-        this.pagedCiphers = [];
-        this.loadMore();
     }
 }
