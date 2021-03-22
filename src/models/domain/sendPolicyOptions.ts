7a1916841758f47a734ea71f454ffb55b6c2f313
Thomas Rittson <trittson@bitwarden.com>
2021-03-22 14:57:54 +1000

    Delete unnecessary model

--- src/models/domain/sendPolicyOptions.ts
+++
@@ -1,6 +0,0 @@
-import { DisableSendPolicyType } from '../../enums/disableSendPolicyType';
-import Domain from './domainBase';
-
-export class DisableSendPolicyOptions extends Domain {
-    disabledSendTypes: DisableSendPolicyType
-}
