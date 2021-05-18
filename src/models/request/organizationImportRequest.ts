aaee69081b658ba4bfd7d133182bf3186b3b4e98
Matt Gibson <mgibson@bitwarden.com>
2021-05-18 13:06:03 -0500

    Add public import models
    Since public import is tied tightly to the private api, constructors are
    provided to maintain coupling in case of changes

---
+++ src/models/request/organizationImportRequest.ts
@@ -0,0 +1,26 @@
+import { ImportDirectoryRequest } from './importDirectoryRequest';
+import { ImportDirectoryRequestGroup } from './importDirectoryRequestGroup';
+import { OrganizationImportMemberRequest } from './organizationImportedMemberRequest';
+import { OrganizationImportGroupRequest } from './organizationImportGroupRequest';
+
+export class OrganizationImportRequest {
+    groups: OrganizationImportGroupRequest[] = [];
+    members: OrganizationImportMemberRequest[] = [];
+    overwriteExisting: boolean = false;
+    largeImport: boolean = false;
+
+    constructor(model: {
+        groups: Required<OrganizationImportGroupRequest>[],
+        users: Required<OrganizationImportMemberRequest>[], overwriteExisting: boolean, largeImport: boolean;
+    } | ImportDirectoryRequest) {
+        if (model instanceof ImportDirectoryRequest) {
+            this.groups = model.groups.map(g => new OrganizationImportGroupRequest(g));
+            this.members = model.users.map(u => new OrganizationImportMemberRequest(u));
+        } else {
+            this.groups = model.groups.map(g => new OrganizationImportGroupRequest(g));
+            this.members = model.users.map(u => new OrganizationImportMemberRequest(u));
+        }
+        this.overwriteExisting = model.overwriteExisting;
+        this.largeImport = model.largeImport;
+    }
+}
