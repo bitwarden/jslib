ac7b28fafbc1ecf90a214c04436765fd8b912872
Thomas Rittson <trittson@bitwarden.com>
2021-08-11 12:06:17 +1000

    Subclass virtual scroll strategy directive

--- angular/src/directives/CipherListVirtualScroll.directive.ts
+++ angular/src/directives/CipherListVirtualScroll.directive.ts
@@ -1,16 +1,11 @@
 import {
-    coerceNumberProperty,
-    NumberInput,
-} from '@angular/cdk/coercion';
-import {
     FixedSizeVirtualScrollStrategy,
+    CdkFixedSizeVirtualScroll,
     VIRTUAL_SCROLL_STRATEGY,
 } from '@angular/cdk/scrolling';
 import {
     Directive,
     forwardRef,
-    Input,
-    OnChanges,
 } from '@angular/core';
 
 // Custom virtual scroll strategy for cdk-virtual-scroll
@@ -39,48 +34,14 @@ export function _cipherListVirtualScrollStrategyFactory(cipherListDir: CipherLis
 }
 
 @Directive({
-    selector: 'cdk-virtual-scroll-viewport[defaultItemSize]',
+    selector: 'cdk-virtual-scroll-viewport[itemSize]',
     providers: [{
         provide: VIRTUAL_SCROLL_STRATEGY,
         useFactory: _cipherListVirtualScrollStrategyFactory,
         deps: [forwardRef(() => CipherListVirtualScroll)],
     }],
 })
-export class CipherListVirtualScroll implements OnChanges {
-    // tslint:disable
-    static ngAcceptInputType_defaultItemSize: NumberInput;
-    static ngAcceptInputType_minBufferPx: NumberInput;
-    static ngAcceptInputType_maxBufferPx: NumberInput;
-    // tslint:enable
-
-    /** The default size of the items in the list (in pixels). */
-    @Input()
-    get defaultItemSize(): number { return this._defaultItemSize; }
-    set defaultItemSize(value: number) { this._defaultItemSize = coerceNumberProperty(value); }
-    _defaultItemSize = 20;
-
-    /**
-     * The minimum amount of buffer rendered beyond the viewport (in pixels).
-     * If the amount of buffer dips below this number, more items will be rendered. Defaults to 100px.
-     */
-    @Input()
-    get minBufferPx(): number { return this._minBufferPx; }
-    set minBufferPx(value: number) { this._minBufferPx = coerceNumberProperty(value); }
-    _minBufferPx = 100;
-
-    /**
-     * The number of pixels worth of buffer to render for when rendering new items. Defaults to 200px.
-     */
-    @Input()
-    get maxBufferPx(): number { return this._maxBufferPx; }
-    set maxBufferPx(value: number) { this._maxBufferPx = coerceNumberProperty(value); }
-    _maxBufferPx = 200;
-
-    /**
-     * The current size of items in the list (in pixels). This property is updated using checkAndUpdateItemSize.
-     */
-    itemSize: number = this.defaultItemSize;
-
+export class CipherListVirtualScroll extends CdkFixedSizeVirtualScroll {
     checkAndUpdateItemSize = () => {
         const sampleItem = document.querySelector('cdk-virtual-scroll-viewport .virtual-scroll-item') as HTMLElement;
         const newItemSize = sampleItem?.offsetHeight;
@@ -89,10 +50,6 @@ export class CipherListVirtualScroll implements OnChanges {
             this.itemSize = newItemSize;
             this._scrollStrategy.updateItemAndBufferSize(this.itemSize, this.minBufferPx, this.maxBufferPx);
         }
-    }
-
-    ngOnChanges() {
-        this._scrollStrategy.updateItemAndBufferSize(this.itemSize, this.minBufferPx, this.maxBufferPx);
     }
 
     /** The scroll strategy used by this directive. */
