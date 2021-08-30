30419a625fbdeefee1b0d0c24ee1d26064b17ba2
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2021-08-31 06:52:57 +1000

    Move policy checks within policyService (#466)
    * Move policy logic within policyService
    
    * Remove unneeded import
    
    * Clean up unused code
    
    * Fix linting
    
    * Enforce policies from accepting org invite
    
    * Only exempt owner or admin from policies
    
    * Use canManagePolicies as exemption criteria
    
    * Make orgUser status check more semantic
    
    Co-authored-by: Addison Beck <abeck@bitwarden.com>
    
    Co-authored-by: Addison Beck <abeck@bitwarden.com>

--- common/src/abstractions/policy.service.ts
+++ common/src/abstractions/policy.service.ts
@@ -4,16 +4,17 @@ import { MasterPasswordPolicyOptions } from '../models/domain/masterPasswordPoli
 import { Policy } from '../models/domain/policy';
 import { ResetPasswordPolicyOptions } from '../models/domain/resetPasswordPolicyOptions';
 
-import { PolicyType } from '../enums/policyType';
-
 import { ListResponse } from '../models/response/listResponse';
 import { PolicyResponse } from '../models/response/policyResponse';
+
+import { PolicyType } from '../enums/policyType';
 
 export abstract class PolicyService {
     policyCache: Policy[];
 
     clearCache: () => void;
     getAll: (type?: PolicyType) => Promise<Policy[]>;
+    getPolicyForOrganization: (policyType: PolicyType, organizationId: string) => Promise<Policy>;
     replace: (policies: { [id: string]: PolicyData; }) => Promise<any>;
     clear: (userId: string) => Promise<any>;
     getMasterPasswordPolicyOptions: (policies?: Policy[]) => Promise<MasterPasswordPolicyOptions>;
@@ -21,4 +22,5 @@ export abstract class PolicyService {
         enforcedPolicyOptions?: MasterPasswordPolicyOptions) => boolean;
     getResetPasswordPolicyOptions: (policies: Policy[], orgId: string) => [ResetPasswordPolicyOptions, boolean];
     mapPoliciesFromToken: (policiesResponse: ListResponse<PolicyResponse>) => Policy[];
+    policyAppliesToUser: (policyType: PolicyType, policyFilter?: (policy: Policy) => boolean) => Promise<boolean>;
 }
