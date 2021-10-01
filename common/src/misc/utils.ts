562e1fe459e33f633b8c6b501253789318fa8d5a
Matt Gibson <mgibson@bitwarden.com>
2021-10-01 07:50:30 -0500

    Feature/split manage collections permission (#504)
    * Split manage collections permissions
    
    * Convert camel to pascal case for element id -> name

--- common/src/misc/utils.ts
+++ common/src/misc/utils.ts
@@ -325,6 +325,10 @@ export class Utils {
         return url;
     }
 
+    static camelToPascalCase(s: string) {
+        return s.charAt(0).toUpperCase() + s.slice(1);
+    }
+
     private static validIpAddress(ipString: string): boolean {
         // tslint:disable-next-line
         const ipRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
