24fe836032354d4ec39435776e54dd0995e1b389
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2021-10-21 08:13:37 +1000

    Fixes for dynamic modal a11y (#518)
    * Do not close modal if click finishes on background
    
    * Trap tab focus in modals, use ESC to close modal
    
    * Fix Angular change detection errors in modals
    
    * Reset focus on next modal after closing modal
    
    * Minor fixes and linting
    
    * Attach focusTrap to modal-dialog element
    
    * Change mousedown event back to click
    
    * Make topModal private
    
    * Add new div for dismissing modal by clicking bg
    
    * Focus element in modal if no autoFocus directive
    
    * Use backdrop for dismissal
    
    * Fix typo

--- angular/src/directives/autofocus.directive.ts
+++ angular/src/directives/autofocus.directive.ts
@@ -2,7 +2,10 @@ import {
     Directive,
     ElementRef,
     Input,
+    NgZone,
 } from '@angular/core';
+
+import { take } from 'rxjs/operators';
 
 import { Utils } from 'jslib-common/misc/utils';
 
@@ -16,11 +19,15 @@ export class AutofocusDirective {
 
     private autofocus: boolean;
 
-    constructor(private el: ElementRef) { }
+    constructor(private el: ElementRef, private ngZone: NgZone) { }
 
     ngOnInit() {
         if (!Utils.isMobileBrowser && this.autofocus) {
-            this.el.nativeElement.focus();
+            if (this.ngZone.isStable) {
+                this.el.nativeElement.focus();
+            } else {
+                this.ngZone.onStable.pipe(take(1)).subscribe(() => this.el.nativeElement.focus());
+            }
         }
     }
 }
