5f12433040349e916973192cc611e7cf82aee795
Matt Gibson <mgibson@bitwarden.com>
2021-10-22 09:24:35 -0400

    Test agent static version does not change

--- spec/electron/utils.spec.ts
+++ spec/electron/utils.spec.ts
@@ -17,4 +17,11 @@ describe('cleanUserAgent', () => {
         const initialWindowsAgent = `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Bitwarden/${process.version} Chrome/${process.versions.chrome} Electron/${process.versions.electron} Safari/537.36`;
         expect(cleanUserAgent(initialWindowsAgent)).toEqual(expectedUserAgent);
     });
+
+    it('does not change version numbers', () => {
+        const expected = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36`;
+        const initialAgent = `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Bitwarden/1.28.3 Chrome/87.0.4280.141 Electron/11.4.5 Safari/537.36`;
+
+        expect(cleanUserAgent(initialAgent)).toEqual(expected);
+    });
 });
