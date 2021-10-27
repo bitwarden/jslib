be64ab44dc9e990982957954ada4befa6bfed4dd
Thomas Rittson <trittson@bitwarden.com>
2021-10-27 14:55:51 +1000

    Add verifySecret method

--- angular/src/components/verify-master-password.component.ts
+++ angular/src/components/verify-master-password.component.ts
@@ -5,7 +5,12 @@ import {
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { ApiService } from 'jslib-common/abstractions/api.service';
+import { CryptoService } from 'jslib-common/abstractions/crypto.service';
+import { I18nService } from 'jslib-common/abstractions/i18n.service';
+import { PlatformUtilsService } from 'jslib-common/abstractions/platformUtils.service';
 import { UserService } from 'jslib-common/abstractions/user.service';
+
+import { VerifyOtpRequest } from 'jslib-common/models/request/account/verifyOtpRequest';
 
 export type Verification = {
     type: 'MasterPassword' | 'OTP',
@@ -31,7 +36,9 @@ export class VerifyMasterPasswordComponent implements ControlValueAccessor, OnIn
 
     private onChange: (value: Verification) => void;
 
-    constructor(private userService: UserService, private apiService: ApiService) { }
+    constructor(private userService: UserService, private apiService: ApiService,
+        private cryptoService: CryptoService, private platformUtilsService: PlatformUtilsService,
+        private i18nService: I18nService) { }
 
     async ngOnInit() {
         this.usesCryptoAgent = await this.userService.getUsesCryptoAgent();
@@ -53,6 +60,34 @@ export class VerifyMasterPasswordComponent implements ControlValueAccessor, OnIn
             this.disableRequestOtp = true;
             await this.apiService.postAccountRequestOtp();
         }
+    }
+
+    async verifySecret(): Promise<boolean> {
+        const showError = () => this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
+            this.i18nService.t(this.usesCryptoAgent ? 'invalidVerificationCode' : 'invalidMasterPassword'));
+
+        if (this.secret.value == null || this.secret.value === '') {
+            showError();
+            return false;
+        }
+
+        if (this.usesCryptoAgent) {
+            const request = new VerifyOtpRequest(this.secret.value);
+            try {
+                await this.apiService.postAccountVerifyOtp(request);
+            } catch {
+                showError();
+                return false;
+            }
+        } else {
+            const passwordValid = await this.cryptoService.compareAndUpdateKeyHash(this.secret.value, null);
+            if (!passwordValid) {
+                showError();
+                return false;
+            }
+        }
+        
+        return true;
     }
 
     writeValue(obj: any): void {
