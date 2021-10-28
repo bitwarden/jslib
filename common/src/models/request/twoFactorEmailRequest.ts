09638611e055736c5a10b5a38053adc7274b8e75
Thomas Rittson <trittson@bitwarden.com>
2021-10-28 13:55:40 +1000

    Refactor to remove ctor on TwoFactorEmailRequest

--- common/src/models/request/twoFactorEmailRequest.ts
+++ common/src/models/request/twoFactorEmailRequest.ts
@@ -2,10 +2,4 @@ import { PasswordVerificationRequest } from './passwordVerificationRequest';
 
 export class TwoFactorEmailRequest extends PasswordVerificationRequest {
     email: string;
-
-    constructor(email: string, masterPasswordHash: string) {
-        super();
-        this.masterPasswordHash = masterPasswordHash;
-        this.email = email;
-    }
 }
