86ed2baa5df8f56966c1777af26d35cf0e27b124
Thomas Rittson <trittson@bitwarden.com>
2021-11-02 11:54:59 +1000

    Disable reprompt if using crypto agent

--- angular/src/services/passwordReprompt.service.ts
+++ angular/src/services/passwordReprompt.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { PasswordRepromptService as PasswordRepromptServiceAbstraction } from 'jslib-common/abstractions/passwordReprompt.service';
+import { UserService } from 'jslib-common/abstractions/user.service';
 
 import { PasswordRepromptComponent } from '../components/password-reprompt.component';
 import { ModalService } from './modal.service';
@@ -9,13 +10,18 @@ import { ModalService } from './modal.service';
 export class PasswordRepromptService implements PasswordRepromptServiceAbstraction {
     protected component = PasswordRepromptComponent;
 
-    constructor(private modalService: ModalService) { }
+    constructor(private modalService: ModalService, private userService: UserService) { }
 
     protectedFields() {
         return ['TOTP', 'Password', 'H_Field', 'Card Number', 'Security Code'];
     }
 
     async showPasswordPrompt() {
+        const passwordRepromptDisabled = await this.userService.getUsesCryptoAgent();
+        if (passwordRepromptDisabled) {
+            return true;
+        }
+
         const ref = this.modalService.open(this.component, {allowMultipleModals: true});
 
         if (ref == null) {
