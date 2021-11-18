10fa164ffc6f61e2264633ca4874473fa6afb4f4
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2021-11-19 05:40:30 +1000

    Add FirstSsoLogin event (#555)

--- common/src/enums/eventType.ts
+++ common/src/enums/eventType.ts
@@ -47,6 +47,7 @@ export enum EventType {
     OrganizationUser_ResetPassword_Withdraw = 1507,
     OrganizationUser_AdminResetPassword = 1508,
     OrganizationUser_ResetSsoLink = 1509,
+    OrganizationUser_FirstSsoLogin = 1510,
 
     Organization_Updated = 1600,
     Organization_PurgedVault = 1601,
