59a530045860f89bde2cd37e93f4e09d76e307b3
Jake Fink <jfink@bitwarden.com>
2021-12-17 11:24:38 -0500

    move sso properties to globalstate (#583)
    * move sso properties to globalstate
    
    * whitespace
    
    * npm prettier changes

--- common/src/models/domain/account.ts
+++ common/src/models/domain/account.ts
@@ -95,9 +95,6 @@ export class AccountProfile {
   hasPremiumPersonally?: boolean;
   lastActive?: number;
   lastSync?: string;
-  ssoCodeVerifier?: string;
-  ssoOrganizationIdentifier?: string;
-  ssoState?: string;
   userId?: string;
   usesKeyConnector?: boolean;
   keyHash?: string;
