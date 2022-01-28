5d7c1dca1b520918130f6a33c622aaee714ce3fe
Matt Gibson <mgibson@bitwarden.com>
2022-01-28 16:16:25 -0500

    Give up and use simple class testing for angular components

--- angular/spec/components/verify-master-password.component.spec.ts
+++ angular/spec/components/verify-master-password.component.spec.ts
@@ -1,60 +1,24 @@
-// collect all required testing objects for Angular App
-import { TestBed, ComponentFixture, async } from "@angular/core/testing";
-// for Two-Way binding
-import { FormsModule } from '@angular/forms';
+
+import Substitute, { SubstituteOf } from '@fluffy-spoon/substitute';
+
+import { KeyConnectorService } from 'jslib-common/abstractions/keyConnector.service';
+import { UserVerificationService } from 'jslib-common/abstractions/userVerification.service';
 
 import { VerifyMasterPasswordComponent } from '../../src/components/verify-master-password.component';
 
-// define the test suit
-describe('VerifyMasterPasswordComponent', () => {
-  // dfefine the required objects fot test
-  let component: VerifyMasterPasswordComponent;
-  // defining the Component Fixture to monitor changed in component
-  // e.g. DataBinding changes
-  let fixture: ComponentFixture<VerifyMasterPasswordComponent>;
-  // define the HTML element
-  let button: HTMLElement;
-
-  // define the test env. so that the test will be
-  // using Angular standard modules to execute test on component
+describe('VerifyMasterPasswordComponent: simple class tests', () => {
+  let sut: VerifyMasterPasswordComponent;
+  let keyConnectorService: SubstituteOf<KeyConnectorService>;
+  let userVerificationService: SubstituteOf<UserVerificationService>;
 
   beforeEach(() => {
-    // define the TestBedConfiguration
-    TestBed.configureTestingModule({
-      declarations: [VerifyMasterPasswordComponent],
-      imports: [FormsModule]
-    }).compileComponents(); // the component will be compiled
-    // (includes HTML Tremplate)
+    keyConnectorService = Substitute.for<KeyConnectorService>();
+    userVerificationService = Substitute.for<UserVerificationService>();
+
+    sut = new VerifyMasterPasswordComponent(keyConnectorService, userVerificationService);
   });
-  // definition for all objects before test starts
-  beforeEach(() => {
-    // initiaze the fixture so that the component 'selector'
-    // and its HTML template will be initialized
-    fixture = TestBed.createComponent(VerifyMasterPasswordComponent);
-    // read the component's instace to execute method in it
-    component = fixture.componentInstance;
-    // detect the first databinding changes
-    fixture.detectChanges();
-  });
-  // the test case
-  it('should do stuff', () => {
-    expect(component).not.toBe(null);
-    // // define the product instance
-    // const product = new Product(0, '', '', '', '', '', 0);
-    // console.log(`Conponent instance ${component}`);
-    // product.BasePrice = 4000;
-    // component.product = product;
-    // // receive the nativeElement for HTML Template DOM
-    // const element = fixture.nativeElement;
-    // // recive the button
-    // button = element.querySelector('.btn-success');
-    // // define an event
-    // // when the button dispatch the click event the
-    // // 'save()' method of the component will be called
-    // const eventType = button.dispatchEvent(new Event('click'));
-    // // detect any changed in HTML DOM against the dispatched event
-    // fixture.detectChanges();
-    // // assert the value in the disabled text element
-    // expect(element.querySelector('input[disabled]').value).toEqual('800');
-  });
+
+  it('should exist', () => {
+    expect(sut).toBeInstanceOf(VerifyMasterPasswordComponent);
+  })
 });
