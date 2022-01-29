38bc2720b69531ca3b6cda3e98a040e4e50a04b8
Anthony Garera <Gareraanthony@gmail.com>
2022-01-29 17:37:29 -0500

    Refactored code to be in a maintainable dictionary format as per the feedback given on the initial commits PR

--- angular/src/pipes/credit-card-number.pipe.ts
+++ angular/src/pipes/credit-card-number.pipe.ts
@@ -2,147 +2,196 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'creditCardNumber' })
 export class CreditCardNumberPipe implements PipeTransform {
+
+    numberFormats = {
+        Visa: [
+          {
+            cardLength: 16,
+            blocks: [4, 4, 4, 4],
+            startNumber: ['4'],
+            substring: [1],
+          },
+        ],
+        Mastercard: [
+          {
+            cardLength: 16,
+            blocks: [4, 4, 4, 4],
+            startNumber: [
+              ['51', '55'],
+              ['222100', '272099'],
+            ],
+            substring: [2, 6],
+          },
+        ],
+        Maestro: [
+          {
+            cardLength: 16,
+            blocks: [4, 4, 4, 4],
+            startNumber: ['51', '55', '222100', '272099'],
+            substring: [2, 6],
+          },
+          {
+            cardLength: 13,
+            blocks: [4, 4, 5],
+            startNumber: ['51', '55', '222100', '272099'],
+            substring: [2, 6],
+          },
+          {
+            cardLength: 14,
+            blocks: [4, 6, 5],
+            startNumber: ['51', '55', '222100', '272099'],
+            substring: [2, 6],
+          },
+          {
+            cardLength: 19,
+            blocks: [4, 4, 4, 4, 3],
+            startNumber: [
+              ['51', '55'],
+              ['222100', '272099'],
+            ],
+            substring: [2, 6],
+          },
+        ],
+        Discover: [
+            {
+                cardLength: 16,
+                blocks: [4, 4, 4, 4],
+                startNumber: [
+                  ['622126','622925'],
+                  ['644','649'],
+                  ['6011'],
+                  ['65']
+                ],
+                substring: [6,3,4,2],
+              },
+        ],
+        'Diners Club': [
+            {
+                cardLength: 14,
+                blocks: [4, 6, 4],
+                startNumber: [
+                  ['300','305'],
+                  ['38','39'],
+                  ['36'],
+                ],
+                substring: [3,2,2],
+              },
+        ],
+        JCB: [
+            {
+                cardLength: 16,
+                blocks: [4, 4, 4, 4],
+                startNumber: [
+                  ['3528','3589'],
+                ],
+                substring: [4],
+              },
+        ],
+        UnionPay: [
+            {
+                cardLength: 16,
+                blocks: [4, 4, 4, 4],
+                startNumber: [
+                  ['62'],
+                ],
+                substring: [2],
+            },
+            {
+                cardLength: 19,
+                blocks: [6,13],
+                startNumber: [
+                  ['62']
+                ],
+                substring: [2],
+              },
+        ],
+        Amex: [
+            {
+                cardLength: 15,
+                blocks: [4, 6, 5],
+                startNumber: [
+                  ['37'],
+                  ['34']
+                ],
+                substring: [2,2],
+              },
+        ],
+        Other: [
+            {
+                cardLength: 16,
+                blocks: [4,4,4,4],
+                startNumber: [
+                    ['1']
+                ],
+                substring: [1],
+              },
+        ]
+      };
     transform(creditCardNumber: string, brand: string): string {
 
         // See https://baymard.com/checkout-usability/credit-card-patterns for
         // all possible credit card spacing patterns. For now, we just handle
         // the below.
+        let result: string;
+        for (const [key, value] of Object.entries(this.numberFormats)) {
+          if (brand === key) {
+            value.forEach(possibleValue => {
 
-        // Check for Visa #### #### #### #### (4-4-4-4) - Pattern not known for 13-15 and 17-19 digit cards.
-        if (creditCardNumber.length === 16 && brand === 'Visa' && creditCardNumber.substring(0,1) === '4') {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 8),
-                creditCardNumber.slice(8, 12),
-                creditCardNumber.slice(12),
-            ].join('-');
-        }
-        // Check for MasterCard #### #### #### #### (4-4-4-4)
-        if (creditCardNumber.length === 16 && brand === 'MasterCard' && (this.inRange(creditCardNumber.substring(0,2),51,55) || this.inRange(creditCardNumber.substring(0,6),222100,272099))) {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 8),
-                creditCardNumber.slice(8, 12),
-                creditCardNumber.slice(12),
-            ].join('-');
-        }
-
-        // Check for Maestro #### #### ##### (4-4-5) #### ###### ##### (4-6-5) #### #### #### #### (4-4-4-4) #### #### #### #### ### (4-4-4-4-3)
-        if (brand === 'Maestro' && (this.inRange(creditCardNumber.substring(0,6),500000,509999) || this.inRange(creditCardNumber.substring(0,6),560000,589999) || this.inRange(creditCardNumber.substring(0,6),600000,699999))) {
-            if (creditCardNumber.length === 13) {
-                return [
-                    creditCardNumber.slice(0,4),
-                    creditCardNumber.slice(4,8),
-                    creditCardNumber.slice(8),
-                ].join('-');
-            }
-            if (creditCardNumber.length === 15) {
-                return [
-                    creditCardNumber.slice(0, 4),
-                    creditCardNumber.slice(4, 10),
-                    creditCardNumber.slice(10),
-                ].join('-');
-            }
-            if (creditCardNumber.length === 16) {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 8),
-                creditCardNumber.slice(8, 12),
-                creditCardNumber.slice(12),
-            ].join('-');
-            }
-            if (creditCardNumber.length === 19) {
+              if (possibleValue.cardLength === creditCardNumber.length) {
+                possibleValue.startNumber.forEach((startNumberData, i) => {
+                  if (
+                    this.inRange(
+                      creditCardNumber.substring(0, possibleValue.substring[i]),
+                      startNumberData[0],
+                      startNumberData[1]
+                    ) === true
+                  ) {
+                    const preFormattedCardValue: string[] = [];
+                    let previousSlice = 0;
+                    possibleValue.blocks.forEach((block, i) => {
+                      const total = (block += previousSlice);
+                      preFormattedCardValue.push(
+                        creditCardNumber.slice(previousSlice, total)
+                      );
+                      previousSlice = block;
+                    });
+                    result = preFormattedCardValue.join('-');
+                  }
+                });
+              }
+            });
+            if (brand === 'Other') {
                 return [
                     creditCardNumber.slice(0, 4),
                     creditCardNumber.slice(4, 8),
                     creditCardNumber.slice(8, 12),
-                    creditCardNumber.slice(12,16),
-                    creditCardNumber.slice(16),
+                    creditCardNumber.slice(12),
                 ].join('-');
-                }
+            }
+          }
         }
-        // Check for Discover
-        if (creditCardNumber.length === 16 && brand === 'Discover' && (this.inRange(creditCardNumber.substring(0,6),622126,622925) || this.inRange(creditCardNumber.substring(0,3),644,649)  || creditCardNumber.substring(0,4) === '6011' || creditCardNumber.substring(0,2) === '65' )) {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 8),
-                creditCardNumber.slice(8, 12),
-                creditCardNumber.slice(12),
-            ].join('-');
+        if (result === undefined) {
+            return creditCardNumber;
         }
-        // Check for Diners Club 14 digit - #### ###### #### (4-6-4)
-        if (creditCardNumber.length === 14 && brand === 'Diners Club' && (this.inRange(creditCardNumber.substring(0,3),300,305) || this.inRange(creditCardNumber.substring(0,2),38,39) || creditCardNumber.substring(0,2) === '36' )) {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 10),
-                creditCardNumber.slice(10),
-            ].join('-');
-        }
-
-        // Check for Diners Club 16 Digit - #### #### #### #### (4-4-4-4)
-        if (creditCardNumber.length === 16 && brand === 'Diners Club' && (creditCardNumber.substring(0,2) === '54' || creditCardNumber.substring(0,2) === '55')) {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 8),
-                creditCardNumber.slice(8, 12),
-                creditCardNumber.slice(12),
-            ].join('-');
-        }
-
-        // Check for JCB 16 digit - #### #### #### #### (4-4-4-4)
-        if (creditCardNumber.length === 16 && brand === 'JCB' && (this.inRange(creditCardNumber.substring(0,4),3528,3589))) {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 8),
-                creditCardNumber.slice(8, 12),
-                creditCardNumber.slice(12),
-            ].join('-');
-        }
-        // China UnionPay 16 digit pattern - #### #### #### #### (4-4-4-4) Pattern not known for 17-18 digit cards.
-        if (creditCardNumber.length === 16 && brand === 'UnionPay' && creditCardNumber.substring(0,2) === '62') {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 8),
-                creditCardNumber.slice(8, 12),
-                creditCardNumber.slice(12),
-            ].join('-');
-        }
-        // China UnionPay 19 digit pattern - ###### ############# (6-13) Pattern not known for 17-18 digit cards.
-        if (creditCardNumber.length === 19 && brand === 'UnionPay' && creditCardNumber.substring(0,2) === '62') {
-            return [
-                creditCardNumber.slice(0, 6),
-                creditCardNumber.slice(6),
-            ].join('-');
-        }
-
-        // American Express is 15 digits in length making the pattern - #### ###### ##### (4-6-5)
-        if (creditCardNumber.length === 15 && brand === 'Amex' && (creditCardNumber.substring(0,2) === '37' || creditCardNumber.substring(0,2) === '34')) {
-            return [
-                creditCardNumber.slice(0, 4),
-                creditCardNumber.slice(4, 10),
-                creditCardNumber.slice(10),
-            ].join('-');
-        }
-
-        return creditCardNumber;
-    }
-
-    /**
-     *
-     * @param x this is the card number value that you want to compare to see if it fits the range.
-     * @param min The minimum value, greater than or equal to.
-     * @param max The maximum value, less than or equal to.
-     * @returns boolean true or false value.
-     */
-    private inRange(x: string, min: number, max: number) {
-        // Converting string to a number
-        const xAsANumber = parseFloat(x);
-        if( min <= xAsANumber && xAsANumber <= max) {
-            return true;
-        } else {
-            return false;
-        }
-
+        return result;
 
     }
+
+  /**
+   *
+   * @param x this is the card number value that you want to compare to see if it fits the range.
+   * @param min The minimum value, greater than or equal to.
+   * @param max The maximum value, less than or equal to.
+   * @returns boolean true or false value.
+   */
+   private inRange(x: string, min: string, max?: string) {
+    // Converting string to a number
+    const xAsANumber = parseFloat(x);
+
+    if ((parseFloat(min) <= xAsANumber && xAsANumber <= parseFloat(max)) || parseFloat(min) === xAsANumber) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
