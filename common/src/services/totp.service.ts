220020844c1b6651f14554c3978ea2a3709c2da7
Hinton <oscar@oscarhinton.com>
2022-02-03 19:13:58 +0100

    Remove tslint remnants

--- common/src/services/totp.service.ts
+++ common/src/services/totp.service.ts
@@ -76,14 +76,12 @@ export class TotpService implements TotpServiceAbstraction {
       return null;
     }
 
-    /* tslint:disable */
     const offset = hash[hash.length - 1] & 0xf;
     const binary =
       ((hash[offset] & 0x7f) << 24) |
       ((hash[offset + 1] & 0xff) << 16) |
       ((hash[offset + 2] & 0xff) << 8) |
       (hash[offset + 3] & 0xff);
-    /* tslint:enable */
 
     let otp = "";
     if (isSteamAuth) {
