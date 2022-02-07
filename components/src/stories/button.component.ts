f81bd299d76a3d11e75c6636757771d728826efe
Hinton <oscar@oscarhinton.com>
2022-02-07 09:40:11 +0100

    Change button to be a directive

--- components/src/stories/button.component.ts
+++ components/src/stories/button.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  HostBinding,
+  ElementRef,
+  OnChanges,
+  Directive,
+} from "@angular/core";
 
 type ModeTypes = "primary" | "secondary";
 type ButtonTypes = "default" | "outline";
@@ -6,20 +15,20 @@ type CombiendTypes = `${ModeTypes}-${ButtonTypes}`;
 
 const buttonStyles: Record<CombiendTypes, string> = {
   "primary-default":
-    "tw-border tw-border-blue-500 tw-bg-blue-500 tw-text-white hover:tw-bg-blue-700",
+    "tw-border-primary-500 tw-bg-primary-500 tw-text-white hover:tw-bg-primary-700 disabled:tw-opacity-60 disabled:hover:tw-bg-primary-500",
   "primary-outline":
-    "tw-border tw-border-blue-500 tw-text-blue-500 hover:tw-bg-blue-500 hover:tw-text-white",
+    "tw-border-primary-500 tw-text-primary-500 hover:tw-bg-primary-500 hover:tw-text-white",
   "secondary-default": "",
-  "secondary-outline": "",
+  "secondary-outline":
+    "tw-border-secondary-500 !tw-text-secondary-900 hover:tw-bg-secondary-500 hover:!tw-text-[#333333]",
 };
 
-@Component({
-  selector: "storybook-button",
-  template: `<button type="button" (click)="onClick.emit($event)" [ngClass]="classes">
-    {{ label }}
-  </button>`,
+@Directive({
+  selector: "button[bit-button], a[bit-button]",
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnChanges {
+  @HostBinding("class") @Input("class") classList: string = "";
+
   /**
    * Is this the principal call to action on the page?
    */
@@ -29,6 +38,12 @@ export class ButtonComponent {
   @Input()
   buttonType: ButtonTypes = "default";
 
+  @Input()
+  block = false;
+
+  @Input()
+  type: "button" | "submit" = "button";
+
   /**
    * How large should the button be?
    */
@@ -36,24 +51,23 @@ export class ButtonComponent {
   size: "small" | "medium" | "large" = "medium";
 
   /**
-   * Button contents
-   *
-   * @required
-   */
-  @Input()
-  label = "Button";
-
-  /**
    * Optional click handler
    */
   @Output()
   onClick = new EventEmitter<Event>();
 
+  constructor(elementRef: ElementRef) {}
+
+  ngOnChanges() {
+    this.classList = this.classes.join(" ");
+  }
+
   public get classes(): string[] {
     const style: CombiendTypes = `${this.mode}-${this.buttonType}`;
 
     return [
-      "tw-font-semibold tw-py-2 tw-px-4 tw-rounded tw-mr-2 tw-transition",
+      "tw-font-semibold tw-py-2 tw-px-4 tw-rounded tw-transition tw-border tw-border-solid tw-text-center hover:tw-no-underline",
+      this.block ? "tw-w-full tw-block" : "",
       buttonStyles[style],
     ];
   }
