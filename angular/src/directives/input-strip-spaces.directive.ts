da8c480c95ce7786141faf93a1b8f849aa690475
Thomas Rittson <trittson@bitwarden.com>
2022-02-23 07:41:00 +1000

    Fix selector

--- angular/src/directives/input-strip-spaces.directive.ts
+++ angular/src/directives/input-strip-spaces.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, HostListener } from "@angular/core";
 
 @Directive({
-  selector: "input[appChangeStripSpaces]",
+  selector: "input[appInputStripSpaces]",
 })
 export class InputStripSpacesDirective {
   constructor(private el: ElementRef<HTMLInputElement>) {}
