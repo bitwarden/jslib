0642073095aaa5b06c69530371e44e884f931dc9
Thomas Rittson <trittson@bitwarden.com>
2022-03-01 10:27:08 +1000

    Revert "Fix default valuess in autofill settings migration"
    This reverts commit 32bccb8e9ae67ba32738220cb4c0c9cbb87831a3.

--- common/src/models/domain/account.ts
+++ common/src/models/domain/account.ts
@@ -100,7 +100,7 @@ export class AccountProfile {
 
 export class AccountSettings {
   autoConfirmFingerPrints?: boolean;
-  autoFillOnPageLoadDefault?: boolean = true;
+  autoFillOnPageLoadDefault?: boolean;
   biometricLocked?: boolean;
   biometricUnlock?: boolean;
   clearClipboard?: number;
@@ -116,7 +116,7 @@ export class AccountSettings {
   dontShowCardsCurrentTab?: boolean;
   dontShowIdentitiesCurrentTab?: boolean;
   enableAlwaysOnTop?: boolean;
-  enableAutoFillOnPageLoad?: boolean = false;
+  enableAutoFillOnPageLoad?: boolean;
   enableBiometric?: boolean;
   enableFullWidth?: boolean;
   enableGravitars?: boolean;
