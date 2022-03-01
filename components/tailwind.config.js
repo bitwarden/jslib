6eed64e8ed957c7091540e5937144a0307a669a5
Hinton <oscar@oscarhinton.com>
2022-03-01 20:22:51 +0100

    Move tailwind config to jslib

--- components/tailwind.config.js
+++ components/tailwind.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const config = require("../../tailwind.config");
+const config = require("./tailwind.config.base");
 
 config.content = ["./src/**/*.{html,ts}", "./.storybook/preview.js"];
 
