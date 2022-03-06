25dff306f8e9174dbed0fb1f7b915001647eb36f
Daniel James Smith <djsmith@web.de>
2022-03-06 14:17:13 +0100

    Add test suite for existing importer

---
+++ spec/common/importers/mykiCsvImporter.spec.ts
@@ -0,0 +1,87 @@
+import { CipherType } from "jslib-common/enums/cipherType";
+import { MykiCsvImporter as Importer } from "jslib-common/importers/MykiCsvImporter";
+
+import { userAccountData } from "./testData/mykiCsv/UserAccount.csv";
+import { userCreditCardData } from "./testData/mykiCsv/UserCreditCard.csv";
+import { userIdentityData } from "./testData/mykiCsv/UserIdentity.csv";
+import { userNoteData } from "./testData/mykiCsv/UserNote.csv";
+
+describe("Myki CSV Importer", () => {
+  let importer: Importer;
+  beforeEach(() => {
+    importer = new Importer();
+  });
+
+  it("should parse userAccount records", async () => {
+    const result = await importer.parse(userAccountData);
+    expect(result != null).toBe(true);
+
+    const cipher = result.ciphers.shift();
+
+    expect(cipher.name).toEqual("PasswordNickname");
+    expect(cipher.login.username).toEqual("user.name@email.com");
+    expect(cipher.login.password).toEqual("abc123");
+    expect(cipher.login.totp).toEqual("someTOTPSeed");
+    expect(cipher.login.uris.length).toEqual(1);
+    const uriView = cipher.login.uris.shift();
+    expect(uriView.uri).toEqual("http://www.google.com");
+    expect(cipher.notes).toEqual("This is the additional information text.");
+  });
+
+  it("should parse creditCard records", async () => {
+    const result = await importer.parse(userCreditCardData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+    expect(result.ciphers.length).toBe(1);
+
+    const cipher = result.ciphers.shift();
+    expect(cipher.type).toBe(CipherType.Card);
+    expect(cipher.name).toBe("Visa test card");
+    expect(cipher.card.brand).toBe("Visa");
+    expect(cipher.card.cardholderName).toBe("Joe User");
+    expect(cipher.card.number).toBe("4111111111111111");
+    expect(cipher.card.code).toBe("222");
+    expect(cipher.card.expMonth).toBe("04");
+    expect(cipher.card.expYear).toBe("24");
+
+    expect(cipher.notes).toBe("This is the additional information field");
+  });
+
+  it("should parse identity records", async () => {
+    const result = await importer.parse(userIdentityData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+
+    const cipher = result.ciphers.shift();
+    expect(cipher.type).toBe(CipherType.Identity);
+    expect(cipher.name).toBe("Joe User's nickname");
+    expect(cipher.identity.fullName).toBe("Mr Joe M User");
+    expect(cipher.identity.title).toBe("Mr");
+    expect(cipher.identity.firstName).toBe("Joe");
+    expect(cipher.identity.middleName).toBe("M");
+    expect(cipher.identity.lastName).toBe("User");
+    expect(cipher.identity.email).toBe("joe.user@email.com");
+
+    expect(cipher.identity.address1).toBe("1 Example House");
+    expect(cipher.identity.address2).toBe("Suite 300");
+
+    expect(cipher.identity.city).toBe("Portland");
+    expect(cipher.identity.postalCode).toBe("04101");
+    expect(cipher.identity.country).toBe("United States");
+  });
+
+  it("should parse secureNote records", async () => {
+    const result = await importer.parse(userNoteData);
+
+    expect(result).not.toBeNull();
+    expect(result.success).toBe(true);
+    expect(result.ciphers.length).toBe(1);
+
+    const cipher = result.ciphers.shift();
+    expect(cipher.type).toBe(CipherType.SecureNote);
+    expect(cipher.name).toBe("The title of a secure note");
+    expect(cipher.notes).toBe("The content of a secure note. Lorem ipsum, etc.");
+  });
+});
