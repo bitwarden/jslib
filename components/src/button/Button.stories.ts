67a4fc85910645f2d05031f7c7d29342475e4648
Oscar Hinton <oscar@oscarhinton.com>
2022-03-08 11:50:34 +0100

    Component Library scaffolding (#625)

---
+++ components/src/button/Button.stories.ts
@@ -0,0 +1,49 @@
+import { Meta, Story } from "@storybook/angular";
+
+import { ButtonComponent } from "./button.component";
+
+// More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
+export default {
+  title: "Jslib/Button",
+  component: ButtonComponent,
+  args: {
+    buttonType: "primary",
+  },
+  // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
+const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
+  props: args,
+  template: `<button bit-button [buttonType]="buttonType" [block]="block">Test</button>`,
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/angular/writing-stories/args
+Primary.args = {
+  buttonType: "primary",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  buttonType: "secondary",
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  buttonType: "danger",
+};
+
+const DisabledTemplate: Story = (args) => ({
+  props: args,
+  template: `
+    <button bit-button disabled buttonType="primary" class="tw-mr-2">Primary</button>
+    <button bit-button disabled buttonType="secondary" class="tw-mr-2">Secondary</button>
+    <button bit-button disabled buttonType="danger" class="tw-mr-2">Danger</button>
+  `,
+});
+
+export const Disabled = DisabledTemplate.bind({});
+Disabled.args = {
+  size: "small",
+};
---
+++ components/tailwind.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+const config = require("./tailwind.config.base");
+
+config.content = ["./src/**/*.{html,ts}", "./.storybook/preview.js"];
+
+module.exports = config;
