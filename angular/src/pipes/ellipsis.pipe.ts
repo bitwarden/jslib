ce539a9ce4bb08d9c7a84ce98ba1cb051ecefa47
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-03-16 16:46:20 -0400

    add ellipsis pipe

---
+++ angular/src/pipes/ellipsis.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'ellipsis'
+})
+export class EllipsisPipe implements PipeTransform {
+  transform(value: string, limit = 25, completeWords = false, ellipsis = '...') {
+    if (completeWords) {
+      limit = value.substring(0, limit).lastIndexOf(' ');
+    }
+    return value.length > limit ? value.substring(0, limit) + ellipsis : value;
+  }
+}
