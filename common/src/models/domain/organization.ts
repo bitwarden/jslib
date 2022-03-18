6dc1d65abc9c178373b3a51edd37b5167cbd83e3
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2022-03-18 17:28:37 +1000

    [End User Vault Refresh] Refactor route permission checking (#727)
    * Update admin access logic
    
    * Centralize route permission handling
    
    * Add permission check for disabled orgs

--- common/src/models/domain/organization.ts
+++ common/src/models/domain/organization.ts
@@ -1,5 +1,6 @@
 import { OrganizationUserStatusType } from "../../enums/organizationUserStatusType";
 import { OrganizationUserType } from "../../enums/organizationUserType";
+import { Permissions } from "../../enums/permissions";
 import { ProductType } from "../../enums/productType";
 import { PermissionsApi } from "../api/permissionsApi";
 import { OrganizationData } from "../data/organizationData";
@@ -182,26 +183,25 @@ export class Organization {
     return this.canManagePolicies;
   }
 
-  get canAccessManageTab(): boolean {
-    return (
-      this.canManageUsers ||
-      this.canViewAllCollections ||
-      this.canViewAssignedCollections ||
-      this.canManageGroups ||
-      this.canManagePolicies ||
-      this.canAccessEventLogs
-    );
-  }
+  hasAnyPermission(permissions: Permissions[]) {
+    const specifiedPermissions =
+      (permissions.includes(Permissions.AccessEventLogs) && this.canAccessEventLogs) ||
+      (permissions.includes(Permissions.AccessImportExport) && this.canAccessImportExport) ||
+      (permissions.includes(Permissions.AccessReports) && this.canAccessReports) ||
+      (permissions.includes(Permissions.CreateNewCollections) && this.canCreateNewCollections) ||
+      (permissions.includes(Permissions.EditAnyCollection) && this.canEditAnyCollection) ||
+      (permissions.includes(Permissions.DeleteAnyCollection) && this.canDeleteAnyCollection) ||
+      (permissions.includes(Permissions.EditAssignedCollections) &&
+        this.canEditAssignedCollections) ||
+      (permissions.includes(Permissions.DeleteAssignedCollections) &&
+        this.canDeleteAssignedCollections) ||
+      (permissions.includes(Permissions.ManageGroups) && this.canManageGroups) ||
+      (permissions.includes(Permissions.ManageOrganization) && this.isOwner) ||
+      (permissions.includes(Permissions.ManagePolicies) && this.canManagePolicies) ||
+      (permissions.includes(Permissions.ManageUsers) && this.canManageUsers) ||
+      (permissions.includes(Permissions.ManageUsersPassword) && this.canManageUsersPassword) ||
+      (permissions.includes(Permissions.ManageSso) && this.canManageSso);
 
-  get canAccessToolsTab(): boolean {
-    return this.canAccessImportExport || this.canAccessReports;
-  }
-
-  get canAccessSettingsTab(): boolean {
-    return this.isOwner;
-  }
-
-  get canAccessAdminView() {
-    return this.canAccessManageTab || this.canAccessToolsTab || this.canAccessSettingsTab;
+    return specifiedPermissions && (this.enabled || this.isOwner);
   }
 }
