937d18e1a0c147b1cc3808ca8bd578d62eabc007
Matt Gibson <mgibson@bitwarden.com>
2022-03-17 21:40:42 -0400

    Remove error Response type check
    Minimization is impacting type checking in a non-consistent way.
    The previous type check works locally,
    but not from build artifacts :shrug:. We only set `captchaRequired` on
    our errors when we want a resubmit with captcha included, so we're safe
    keying off that

--- angular/src/directives/api-action.directive.ts
+++ angular/src/directives/api-action.directive.ts
@@ -15,7 +15,7 @@ export class ApiActionDirective implements OnChanges {
     private el: ElementRef,
     private validationService: ValidationService,
     private logService: LogService
-  ) {}
+  ) { }
 
   ngOnChanges(changes: any) {
     if (this.appApiAction == null || this.appApiAction.then == null) {
@@ -31,10 +31,7 @@ export class ApiActionDirective implements OnChanges {
       (e: any) => {
         this.el.nativeElement.loading = false;
 
-        if (
-          (e instanceof ErrorResponse || e.constructor.name === ErrorResponse.name) &&
-          (e as ErrorResponse).captchaRequired
-        ) {
+        if ((e as ErrorResponse).captchaRequired) {
           this.logService.error("Captcha required error response: " + e.getSingleMessage());
           return;
         }
