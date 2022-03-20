af57106bade3a0ff444e9d55f3ac97b35c11f5b3
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-03-19 20:57:43 -0400

    Fix complete words case

--- angular/src/pipes/ellipsis.pipe.ts
+++ angular/src/pipes/ellipsis.pipe.ts
@@ -5,9 +5,10 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class EllipsisPipe implements PipeTransform {
   transform(value: string, limit = 25, completeWords = false, ellipsis = "...") {
+    limit -= ellipsis.length;
     if (completeWords) {
       limit = value.substring(0, limit).lastIndexOf(" ");
     }
-    return value.length > limit ? value.substring(0, limit - ellipsis.length) + ellipsis : value;
+    return value.length > limit ? value.substring(0, limit) + ellipsis : value;
   }
 }
