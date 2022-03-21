0a6784a98d343738e5d4695ebd1753661c2322fa
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-03-21 12:40:48 -0400

    fix for if there are not spaces in long value

--- angular/src/pipes/ellipsis.pipe.ts
+++ angular/src/pipes/ellipsis.pipe.ts
@@ -7,7 +7,9 @@ export class EllipsisPipe implements PipeTransform {
   transform(value: string, limit = 25, completeWords = false, ellipsis = "...") {
     limit -= ellipsis.length;
     if (completeWords && value.length > limit) {
-      limit = value.substring(0, limit).lastIndexOf(" ");
+      if (value.indexOf(" ") > 0) {
+        limit = value.substring(0, limit).lastIndexOf(" ");
+      }
     }
     return value.length > limit ? value.substring(0, limit) + ellipsis : value;
   }
