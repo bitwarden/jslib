b2690aee06065473e58eb3e063fb1574d5a057a5
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-03-21 13:48:47 -0400

    extract length check to beginning of method

--- angular/src/pipes/ellipsis.pipe.ts
+++ angular/src/pipes/ellipsis.pipe.ts
@@ -5,12 +5,15 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class EllipsisPipe implements PipeTransform {
   transform(value: string, limit = 25, completeWords = false, ellipsis = "...") {
+    if (value.length <= limit) {
+      return value;
+    }
     limit -= ellipsis.length;
     if (completeWords && value.length > limit) {
       if (value.indexOf(" ") > 0) {
         limit = value.substring(0, limit).lastIndexOf(" ");
       }
     }
-    return value.length > limit ? value.substring(0, limit) + ellipsis : value;
+    return value.substring(0, limit) + ellipsis;
   }
 }
