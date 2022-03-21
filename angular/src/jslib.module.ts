91f264949bfece399aa649bf3692394cfe8b1947
Thomas Rittson <trittson@bitwarden.com>
2022-03-21 17:54:06 +1000

    Fix linting

--- angular/src/jslib.module.ts
+++ angular/src/jslib.module.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { CommonModule } from '@angular/common';
 
+import { AvatarComponent } from "./components/avatar.component";
+import { CalloutComponent } from "./components/callout.component";
+import { ExportScopeCalloutComponent } from "./components/export-scope-callout.component";
+import { IconComponent } from "./components/icon.component";
+import { BitwardenToastModule } from "./components/toastr.component";
+import { VerifyMasterPasswordComponent } from "./components/verify-master-password.component";
 import { A11yInvalidDirective } from "./directives/a11y-invalid.directive";
 import { A11yTitleDirective } from "./directives/a11y-title.directive";
 import { ApiActionDirective } from "./directives/api-action.directive";
-import { AvatarComponent } from "./components/avatar.component";
 import { AutofocusDirective } from "./directives/autofocus.directive";
 import { BlurClickDirective } from "./directives/blur-click.directive";
 import { BoxRowDirective } from "./directives/box-row.directive";
@@ -21,13 +26,6 @@ import { I18nPipe } from "./pipes/i18n.pipe";
 import { SearchCiphersPipe } from "./pipes/search-ciphers.pipe";
 import { SearchPipe } from "./pipes/search.pipe";
 import { UserNamePipe } from "./pipes/user-name.pipe";
-
-import { CalloutComponent } from "./components/callout.component";
-import { ExportScopeCalloutComponent } from "./components/export-scope-callout.component";
-import { IconComponent } from "./components/icon.component";
-import { VerifyMasterPasswordComponent } from "./components/verify-master-password.component";
-import { BitwardenToastModule } from './components/toastr.component';
-
 
 @NgModule({
   imports: [
@@ -91,9 +89,6 @@ import { BitwardenToastModule } from './components/toastr.component';
     VerifyMasterPasswordComponent,
     ExportScopeCalloutComponent,
   ],
-  providers: [
-    UserNamePipe,
-    SearchPipe,
-  ],
+  providers: [UserNamePipe, SearchPipe],
 })
 export class JslibModule {}
