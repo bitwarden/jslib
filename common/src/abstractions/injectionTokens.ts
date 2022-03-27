2541ab97917ae069dab872b738ca2a1b940f1ca7
Thomas Rittson <trittson@gmail.com>
2022-03-27 14:34:45 +1000

    Add ClientType token

--- common/src/abstractions/injectionTokens.ts
+++ common/src/abstractions/injectionTokens.ts
@@ -8,3 +8,4 @@ export const STATE_SERVICE_USE_CACHE = new InjectionToken<boolean>("STATE_SERVIC
 export const STATE_FACTORY = new InjectionToken<StateFactory>("STATE_FACTORY");
 export const SECURE_STORAGE = new InjectionToken<StorageService>("SECURE_STORAGE");
 export const WINDOW_TOKEN = new InjectionToken<Window>("WINDOW");
+export const CLIENT_TYPE = new InjectionToken<Window>("CLIENT_TYPE");
