25c10a98a5df3f47bd5af7d65ae7e27f599126db
Thomas Rittson <trittson@bitwarden.com>
2022-03-28 13:47:36 +1000

    Run prettier

--- common/src/misc/injectionTokens.ts
+++ common/src/misc/injectionTokens.ts
@@ -3,7 +3,6 @@ import { InjectionToken } from "@angular/core";
 import { StorageService } from "../abstractions/storage.service";
 import { StateFactory } from "../factories/stateFactory";
 
-
 export const STATE_SERVICE_USE_CACHE = new InjectionToken<boolean>("STATE_SERVICE_USE_CACHE");
 export const STATE_FACTORY = new InjectionToken<StateFactory>("STATE_FACTORY");
 export const SECURE_STORAGE = new InjectionToken<StorageService>("SECURE_STORAGE");
