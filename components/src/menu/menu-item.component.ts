242b8fd213183f33dae74c7e7fc7bcc121df53b1
Thomas Rittson <trittson@bitwarden.com>
2022-04-07 15:29:16 +1000

    Improve styling

--- components/src/menu/menu-item.component.ts
+++ components/src/menu/menu-item.component.ts
@@ -1,10 +1,10 @@
-import { Component, Output, TemplateRef, ViewChild, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: "[bit-menu-item]",
     templateUrl: "./menu-item.component.html",
     host: {
-        "class": "tw-py-2 tw-px-5"
+        "class": "tw-py-1 tw-px-6 !tw-text-main !tw-no-underline hover:tw-bg-secondary-100"
     }
 })
 export class MenuItemComponent {
