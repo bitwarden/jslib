78563830da9a0c82ecfae0f517a56f2f6f2076b2
Thomas Rittson <trittson@bitwarden.com>
2022-04-10 08:17:42 +1000

    Remove unused import

--- angular/src/services/lock-guard.service.ts
+++ angular/src/services/lock-guard.service.ts
@@ -2,18 +2,13 @@ import { Injectable } from "@angular/core";
 import { CanActivate, Router } from "@angular/router";
 
 import { AuthService } from "jslib-common/abstractions/auth.service";
-import { StateService } from "jslib-common/abstractions/state.service";
 import { AuthenticationStatus } from "jslib-common/enums/authenticationStatus";
 
 @Injectable()
 export class LockGuardService implements CanActivate {
   protected homepage = "vault";
   protected loginpage = "login";
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    private stateService: StateService
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   async canActivate() {
     const authStatus = await this.authService.authStatus();
