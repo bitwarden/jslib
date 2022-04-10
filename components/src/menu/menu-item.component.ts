e4426ec8b4db45c5ffae0501c5f2e1b4074241db
Thomas Rittson <trittson@bitwarden.com>
2022-04-10 12:29:01 +1000

    Fix classes string

--- components/src/menu/menu-item.component.ts
+++ components/src/menu/menu-item.component.ts
@@ -1,25 +1,12 @@
 import { FocusableOption } from '@angular/cdk/a11y';
 import { Component, ElementRef } from "@angular/core";
 
-const styles = [
-  "tw-block",
-  "tw-py-1",
-  "tw-px-6",
-  "!tw-text-main",
-  "!tw-no-underline",
-  "hover:tw-bg-secondary-100",
-  "focus:tw-bg-secondary-100",
-  "tw-cursor-pointer",
-  "tw-border-none",
-  "tw-bg-background",
-  "tw-text-left"
-].join(" ");
 
 @Component({
   selector: "[bit-menu-item]",
   templateUrl: "./menu-item.component.html",
   host: {
-    class: styles,
+    class: "tw-block tw-py-1 tw-px-6 !tw-text-main !tw-no-underline hover:tw-bg-secondary-100 focus:tw-bg-secondary-100 tw-cursor-pointer tw-border-none tw-bg-background tw-text-left",
     role: "menuitem",
     tabIndex: "-1"
   },
