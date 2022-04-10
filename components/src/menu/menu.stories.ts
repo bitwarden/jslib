fa2ec1fc4bc9d7999dc6ee758bcf2e6f5a077c2a
Thomas Rittson <trittson@bitwarden.com>
2022-04-10 14:25:54 +1000

    Fix Storybook behaviour

--- components/src/menu/menu.stories.ts
+++ components/src/menu/menu.stories.ts
@@ -6,7 +6,6 @@ import { MenuItemComponent } from "./menu-item.component";
 import { MenuTriggerForDirective } from "./menu-trigger-for.directive";
 import { MenuComponent } from "./menu.component";
 
-// More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
   title: "Jslib/Menu",
   component: MenuTriggerForDirective,
@@ -23,18 +22,22 @@ export default {
   ],
 } as Meta;
 
-// More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
+// This is wrapped in *ngIf to create an Angular template, which scopes the #myMenu template reference variable.
+// Otherwise there are duplicate template reference variables on the page and this causes undefined behaviour.
 const Template: Story<MenuTriggerForDirective> = (args: MenuTriggerForDirective) => ({
   props: args,
-  template: `<button [bitMenuTriggerFor]="myMenu">Test</button>
+  template: `
+  <div *ngIf="true">
+    <button [bitMenuTriggerFor]="myMenu">Test</button>
 
-  <bit-menu #myMenu>
-    <a href="#" bit-menu-item>Anchor link</a>
-    <a href="#" bit-menu-item>Another link</a>
-    <button type="button" bit-menu-item>Button</button>
-    <bit-menu-divider></bit-menu-divider>
-    <button type="button" bit-menu-item>Button after divider</button>
-  </bit-menu>`,
+    <bit-menu #myMenu>
+      <a href="#" bit-menu-item>Anchor link</a>
+      <a href="#" bit-menu-item>Another link</a>
+      <button type="button" bit-menu-item>Button</button>
+      <bit-menu-divider></bit-menu-divider>
+      <button type="button" bit-menu-item>Button after divider</button>
+    </bit-menu>
+  </div>`,
 });
 
 export const Primary = Template.bind({});
