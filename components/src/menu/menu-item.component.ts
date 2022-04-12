895649acfc870c55e3874c634132ebb55d85f094
Thomas Rittson <trittson@bitwarden.com>
2022-04-12 10:13:22 +1000

    Move template into component class

--- components/src/menu/menu-item.component.ts
+++ components/src/menu/menu-item.component.ts
@@ -3,13 +3,13 @@ import { Component, ElementRef } from "@angular/core";
 
 @Component({
   selector: "[bit-menu-item]",
-  templateUrl: "./menu-item.component.html",
   host: {
     class:
       "tw-block tw-py-1 tw-px-6 !tw-text-main !tw-no-underline hover:tw-bg-secondary-100 focus:tw-bg-secondary-100 focus:tw-z-50 tw-cursor-pointer tw-border-none tw-bg-background tw-text-left",
     role: "menuitem",
     tabIndex: "-1",
   },
+  template: `<ng-content></ng-content>`,
 })
 export class MenuItemComponent implements FocusableOption {
   constructor(private elementRef: ElementRef) {}
