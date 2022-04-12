2ba48fba1e744053d85f6a5dd5ceea195a87c8ce
Thomas Rittson <trittson@bitwarden.com>
2022-04-12 11:27:52 +1000

    Standardize focus ring on menu-items

--- components/src/menu/menu-item.component.ts
+++ components/src/menu/menu-item.component.ts
@@ -5,7 +5,10 @@ import { Component, ElementRef } from "@angular/core";
   selector: "[bit-menu-item]",
   host: {
     class:
-      "tw-block tw-py-1 tw-px-6 !tw-text-main !tw-no-underline tw-cursor-pointer tw-border-none tw-bg-background tw-text-left hover:tw-bg-secondary-100 focus:tw-bg-secondary-100 focus:tw-z-50",
+      "tw-block tw-py-1 tw-px-6 !tw-text-main !tw-no-underline tw-cursor-pointer tw-border-none tw-bg-background tw-text-left " +
+      "hover:tw-bg-secondary-100 " +
+      "focus:tw-bg-secondary-100 focus:tw-z-50 focus:tw-outline-none focus:tw-ring focus:tw-ring-offset-2 focus:tw-ring-primary-700 " +
+      "active:!tw-ring-0 active:!tw-ring-offset-0",
     role: "menuitem",
     tabIndex: "-1",
   },
