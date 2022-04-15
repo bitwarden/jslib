a2e4455d3f9d4439d522e3a79a6bd4f5607883dc
Matt Gibson <mgibson@bitwarden.com>
2022-04-15 14:47:39 -0400

    Define org connection request and responses

---
+++ common/src/models/request/organizationConnectionRequest.ts
@@ -0,0 +1,13 @@
+import { OrganizationConnectionType } from 'jslib-common/enums/organizationConnectionType';
+import { BillingSyncConfigRequest } from './billingSyncConfigRequest';
+
+/**API request config types for OrganizationConnectionRequest */
+export type OrganizationConnectionRequestConfigs = BillingSyncConfigRequest;
+
+export class OrganizationConnectionRequest {
+  config: string;
+  
+  constructor(public organizationId: string, public type: OrganizationConnectionType, public enabled: boolean, config: OrganizationConnectionRequestConfigs) {
+    this.config = JSON.stringify(config);
+  }
+}
