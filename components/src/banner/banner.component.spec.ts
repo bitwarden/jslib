7b971174cb894370a20505836f4c6fa3a6c9669d
Hinton <oscar@oscarhinton.com>
2022-04-19 21:40:45 +0200

    Write relevant tests for banner

--- components/src/banner/banner.component.spec.ts
+++ components/src/banner/banner.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { I18nMockService } from "src/utils/i18n-mock.service";
+
+import { I18nService } from "jslib-common/abstractions/i18n.service";
 
 import { BannerComponent } from "./banner.component";
 
@@ -9,16 +12,37 @@ describe("BannerComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BannerComponent],
+      providers: [
+        {
+          provide: I18nService,
+          useFactory: () =>
+            new I18nMockService({
+              warning: "Warning",
+              error: "Error",
+            }),
+        },
+      ],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(BannerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
+  it("should create with alert", () => {
+    expect(component.useAlertRole).toBeTrue();
+    const el = fixture.nativeElement.children[0];
+    expect(el.getAttribute("role")).toEqual("status");
+    expect(el.getAttribute("aria-live")).toEqual("polite");
+  });
+
+  it("useAlertRole=false", () => {
+    component.useAlertRole = false;
+    fixture.autoDetectChanges();
+
+    expect(component.useAlertRole).toBeFalse();
+    const el = fixture.nativeElement.children[0];
+    expect(el.getAttribute("role")).toBeNull();
+    expect(el.getAttribute("aria-live")).toBeNull();
   });
 });
