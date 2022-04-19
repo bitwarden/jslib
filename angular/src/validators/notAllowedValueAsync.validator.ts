5821209d089e53cc1856ca3875d12d76733b33c3
Justin Baur <136baur@gmail.com>
2022-04-19 14:03:13 -0400

    Add in notAllowedValueAsync.validator.ts

---
+++ angular/src/validators/notAllowedValueAsync.validator.ts
@@ -0,0 +1,18 @@
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from "@angular/forms";
+
+export function notAllowedValueAsync(valueGetter: () => Promise<string>, caseInsensitive: boolean = false): AsyncValidatorFn {
+  return async (control: AbstractControl): Promise<ValidationErrors | null> => {
+    let notAllowedValue = await valueGetter();
+    let controlValue = control.value;
+    if (caseInsensitive) {
+      notAllowedValue = notAllowedValue.toLowerCase();
+      controlValue = controlValue.toLowerCase();
+    }
+
+    if (controlValue === notAllowedValue) {
+      return {
+        notAllowedValue: true
+      };
+    }
+  };
+}
