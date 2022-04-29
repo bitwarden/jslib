6dac394190d3a9a01d5554902d8f7519e98a1454
Hinton <oscar@oscarhinton.com>
2022-04-29 16:08:39 +0200

    Add version to cipher

--- common/src/models/domain/cipher2.ts
+++ common/src/models/domain/cipher2.ts
@@ -5,10 +5,20 @@ import { FieldType } from "jslib-common/enums/fieldType";
 import { Attachment } from "./attachment";
 import { EncObject } from "./encObject";
 
+// TODO: We need some way to migrate between versions
+// and ensure the data sent to the server does not match the latest version
+// since we might want to ensure backwards compatability.
+// If the client retrieves a version > known, display an error
+
+// Upgrade this whenever the data model changes.
+export const CIPHER_LATEST_VERSION = 1;
+
 /**
  * New Cipher Model, replaces Cipher.
  */
 export interface Cipher2 {
+  version: number;
+
   id: string;
   organizationId: string;
   type: CipherType;
