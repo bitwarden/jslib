1c3e5473a065dabef010c4cb528f1b778e54fde6
gbubemismith <gsmithwalter@gmail.com>
2022-05-12 23:14:10 +0100

    corrected spelling

--- common/src/services/vaultTimeout.service.ts
+++ common/src/services/vaultTimeout.service.ts
@@ -195,7 +195,7 @@ export class VaultTimeoutService implements VaultTimeoutServiceAbstraction {
     }
 
     //setting custom vault timeout to 0H:0M(immediately) and then compared with the diffSeconds always returns a true
-    //Adding 8 seconds ensures the user is not locked or logged out almost immeditely due to inactivity
+    //Adding 8 seconds ensures the user is not locked or logged out almost immediately due to inactivity
     const vaultTimeoutSeconds = vaultTimeout * 60 === 0 ? 8 : vaultTimeout * 60;
     const diffSeconds = (new Date().getTime() - lastActive) / 1000;
 
