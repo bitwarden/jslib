6b3c5c0c5bff2c532e3e5f27ac7cced3cb6a1c39
André Bispo <abispo@bitwarden.com>
2022-05-17 20:07:53 +0100

    PS-592 Name field is not prioritised in search results - test search method which give priority to matches by Name

--- common/src/services/search.service.ts
+++ common/src/services/search.service.ts
@@ -208,6 +208,43 @@ export class SearchService implements SearchServiceAbstraction {
     });
   }
 
+  searchSendsTest(sends: SendView[], query: string) {
+    query = query.trim().toLocaleLowerCase();
+    if(query === null)
+    {
+      return sends;
+    }
+    console.log("LOG:: searchSends", query);
+    var sendsMatched: SendView[] = new Array();
+    var lowPriorityMatched: SendView[] = new Array();
+    sends.forEach(s => {
+      if (s.name != null && s.name.toLowerCase().indexOf(query) > -1) {
+        sendsMatched.push(s);
+      }
+      if (
+        query.length >= 8 &&
+        (s.id.startsWith(query) ||
+          s.accessId.toLocaleLowerCase().startsWith(query) ||
+          (s.file?.id != null && s.file.id.startsWith(query)))
+      ) {
+        lowPriorityMatched.push(s);
+      }
+      if (s.notes != null && s.notes.toLowerCase().indexOf(query) > -1) {
+        lowPriorityMatched.push(s);
+      }
+      if (s.text?.text != null && s.text.text.toLowerCase().indexOf(query) > -1) {
+        lowPriorityMatched.push(s);
+      }
+      if (s.file?.fileName != null && s.file.fileName.toLowerCase().indexOf(query) > -1) {
+        lowPriorityMatched.push(s);
+      }
+    });
+    console.log("LOG::",sendsMatched);
+    console.log("LOG::",lowPriorityMatched);
+    return sendsMatched.concat(lowPriorityMatched);
+  }
+
+
   getIndexForSearch(): lunr.Index {
     return this.index;
   }
