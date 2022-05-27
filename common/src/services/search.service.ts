3aeb4baa0324d88d84c726e215f16eeb84813141
gbubemismith <gsmithwalter@gmail.com>
2022-05-27 09:30:17 +0100

    made normalization function a static function

--- common/src/services/search.service.ts
+++ common/src/services/search.service.ts
@@ -35,7 +35,7 @@ export class SearchService implements SearchServiceAbstraction {
   }
 
   isSearchable(query: string): boolean {
-    query = Utils.normalizeSearchQuery(query);
+    query = SearchService.normalizeSearchQuery(query);
     const notSearchable =
       query == null ||
       (this.index == null && query.length < this.searchableMinLength) ||
@@ -99,7 +99,7 @@ export class SearchService implements SearchServiceAbstraction {
   ): Promise<CipherView[]> {
     const results: CipherView[] = [];
     if (query != null) {
-      query = Utils.normalizeSearchQuery(query.trim().toLowerCase());
+      query = SearchService.normalizeSearchQuery(query.trim().toLowerCase());
     }
     if (query === "") {
       query = null;
@@ -167,7 +167,7 @@ export class SearchService implements SearchServiceAbstraction {
   }
 
   searchCiphersBasic(ciphers: CipherView[], query: string, deleted = false) {
-    query = Utils.normalizeSearchQuery(query.trim().toLowerCase());
+    query = SearchService.normalizeSearchQuery(query.trim().toLowerCase());
     return ciphers.filter((c) => {
       if (deleted !== c.isDeleted) {
         return false;
@@ -189,7 +189,7 @@ export class SearchService implements SearchServiceAbstraction {
   }
 
   searchSends(sends: SendView[], query: string) {
-    query = Utils.normalizeSearchQuery(query.trim().toLocaleLowerCase());
+    query = SearchService.normalizeSearchQuery(query.trim().toLocaleLowerCase());
     if (query === null) {
       return sends;
     }
@@ -296,9 +296,14 @@ export class SearchService implements SearchServiceAbstraction {
     const checkFields = fields.every((i: any) => searchableFields.includes(i));
 
     if (checkFields) {
-      return Utils.normalizeSearchQuery(token.toString());
+      return SearchService.normalizeSearchQuery(token.toString());
     }
 
     return token;
+  }
+
+  // Remove accents/diacritics characters from text. This regex is equivalent to the Diacritic unicode property escape, i.e. it will match all diacritic characters.
+  static normalizeSearchQuery(query: string): string {
+    return query?.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
   }
 }
